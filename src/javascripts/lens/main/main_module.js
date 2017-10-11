f55cb59a0553a03b20672549f80214a21cd8fb2d
natatat <natuduwela@gmail.com>
2017-10-11 16:55:26 -0700

    Update Getting Started

--- src/javascripts/lens/main/main_module.js
+++ src/javascripts/lens/main/main_module.js
@@ -1,6 +1,8 @@
 import angular from 'angular'
 
 import homeTemplate from './templates/home'
+import gettingStartedTemplate from './templates/getting_started'
 const m = angular.module('lens.main', [
 ])
 m.component('home', { template: homeTemplate })
+m.component('gettingStarted', { template: gettingStartedTemplate })
