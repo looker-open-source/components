636d3fb0d0f429d84daa2d3c6602e622acb8ce6b
Nathan Agrin <n8agrin@gmail.com>
2018-04-09 13:44:24 -0700

    Get styleguidist working

--- stories/buttons.story.tsx
+++ stories/buttons.story.tsx
@@ -1,67 +1,67 @@
-import * as React from 'react'
-import { storiesOf } from "@storybook/react"
-import { withKnobs } from "@storybook/addon-knobs"
-import { action } from '@storybook/addon-actions'
-import { Button, buttonStyles, LookerButtonHTMLAttributes } from '../src/components/buttons/buttons'
-import { ReactElement } from "react"
-import {decapitalize} from '../src/utils/strings'
-
-const stories = storiesOf("Buttons", module)
-stories.addDecorator(withKnobs)
-
-function propsFromStyles (styles: object, prefix: string, includeUndefined = false): Array<string | undefined> {
-  const props: Array<string | undefined> = Object.keys(styles)
-    .filter((key) => key.includes(prefix) && !key.includes('-'))
-    .map((key) => key.replace(new RegExp(`^${prefix}`), ''))
-    .map((key) => decapitalize(key))
-  if (includeUndefined) props.push(undefined)
-  return props
-}
-
-stories.add("Button", () => {
-  const modes = propsFromStyles(buttonStyles, 'mode')
-  const sizes = propsFromStyles(buttonStyles, 'size')
-  modes.push(undefined)
-  sizes.push(undefined)
-  const matrix: ReactElement<LookerButtonHTMLAttributes>[][] = []
-
-  modes.forEach((mode) => {
-    const buttonRow: ReactElement<LookerButtonHTMLAttributes>[] = []
-    sizes.forEach((size) => {
-      let desc = ''
-      switch (size) {
-        case 'extraSmall':
-          desc = 'X-Small'
-          break
-        case 'small':
-          desc = 'Small'
-          break
-        case 'large':
-          desc = 'Large'
-          break
-        default:
-          desc = 'Normal'
-          break
-      }
-      buttonRow.push(<Button onClick={action('clicked')} size={size} mode={mode}>{desc}</Button>)
-    })
-    matrix.push(buttonRow)
-  })
-
-  return (
-    <div>
-    <table>
-      <tbody>
-      {matrix.map((row, idx) => {
-        return (
-          <tr key={idx}>
-            {row.map((button, idx) => (<td key={idx}>{button}</td>))}
-          </tr>
-        )
-      })}
-      </tbody>
-    </table>
-    </div>
-  )
-})
-
+// import * as React from 'react'
+// import { storiesOf } from "@storybook/react"
+// import { withKnobs } from "@storybook/addon-knobs"
+// import { action } from '@storybook/addon-actions'
+// import { Button, buttonStyles, LookerButtonHTMLAttributes } from '../src/components/buttons/buttons'
+// import { ReactElement } from "react"
+// import {decapitalize} from '../src/utils/strings'
+//
+// const stories = storiesOf("Buttons", module)
+// stories.addDecorator(withKnobs)
+//
+// function propsFromStyles (styles: object, prefix: string, includeUndefined = false): Array<string | undefined> {
+//   const props: Array<string | undefined> = Object.keys(styles)
+//     .filter((key) => key.includes(prefix) && !key.includes('-'))
+//     .map((key) => key.replace(new RegExp(`^${prefix}`), ''))
+//     .map((key) => decapitalize(key))
+//   if (includeUndefined) props.push(undefined)
+//   return props
+// }
+//
+// stories.add("Button", () => {
+//   const modes = propsFromStyles(buttonStyles, 'mode')
+//   const sizes = propsFromStyles(buttonStyles, 'size')
+//   modes.push(undefined)
+//   sizes.push(undefined)
+//   const matrix: ReactElement<LookerButtonHTMLAttributes>[][] = []
+//
+//   modes.forEach((mode) => {
+//     const buttonRow: ReactElement<LookerButtonHTMLAttributes>[] = []
+//     sizes.forEach((size) => {
+//       let desc = ''
+//       switch (size) {
+//         case 'extraSmall':
+//           desc = 'X-Small'
+//           break
+//         case 'small':
+//           desc = 'Small'
+//           break
+//         case 'large':
+//           desc = 'Large'
+//           break
+//         default:
+//           desc = 'Normal'
+//           break
+//       }
+//       buttonRow.push(<Button onClick={action('clicked')} size={size} mode={mode}>{desc}</Button>)
+//     })
+//     matrix.push(buttonRow)
+//   })
+//
+//   return (
+//     <div>
+//     <table>
+//       <tbody>
+//       {matrix.map((row, idx) => {
+//         return (
+//           <tr key={idx}>
+//             {row.map((button, idx) => (<td key={idx}>{button}</td>))}
+//           </tr>
+//         )
+//       })}
+//       </tbody>
+//     </table>
+//     </div>
+//   )
+// })
+//
