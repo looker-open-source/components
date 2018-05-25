10f27ceef924e3975101498716d48cb03d04710f
Jared Hardy <jared.hardy@looker.com>
2018-05-25 11:44:15 -0700

    Columns for CardGroup is a bit tricky, lets stick with just gap for now

--- src/components/Card/CardGroup.tsx
+++ src/components/Card/CardGroup.tsx
@@ -4,9 +4,8 @@ import * as styles from './CardGroup.scss'
 
 
 export interface CardGroupProps {
-  columns?: '1' | '2' | '3' | '4' | '5'
-  gap?: 's' | 'm' | 'l' | 'xl'
-  className?: string | undefined
+  gap?: 'xs' | 's' | 'm' | 'l' | 'xl' | '2xl' | '3xl' | '4xl'
+  className?: string
 }
 
 
@@ -14,21 +13,24 @@ type PropertyBag = {
   [key: string]: any
 }
 
-export const CardGroup: React.SFC<CardGroupProps> = ({className, columns, gap, ...args}) => {
-
-  const columnCount = columns? columns : '3'
+/** Utility component that lays `Cards` out on a grid */
+export const CardGroup: React.SFC<CardGroupProps> = ({className, gap, ...args}) => {
   const gapSize = gap? gap : 'l'
 
   const styleableProps: PropertyBag = {
+    [styles.gapXs]: gapSize == 'xs',
     [styles.gapS]: gapSize == 's',
     [styles.gapM]: gapSize == 'm',
     [styles.gapL]: gapSize == 'l',
     [styles.gapXl]: gapSize == 'xl',
+    [styles.gap2xl]: gapSize == '2xl',
+    [styles.gap3xl]: gapSize == '3xl',
+    [styles.gap4xl]: gapSize == '4xl'
   }
 
   return (
-    <div className={classNames(styles.cardGroup, className, styleableProps,)} style={{gridTemplateColumns: `repeat(${columnCount}, 1fr)`}}{...args} >
-
+    <div className={classNames(styles.cardGroup, className, styleableProps,)}{...args} >
+      {args.children}
     </div>
   )
 }
