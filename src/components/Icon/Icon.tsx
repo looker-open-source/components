5f62e70e36bd97cd4c37009f61dcf252ec336c68
Jared Hardy <jared.hardy@looker.com>
2018-06-04 08:46:29 -0700

    Add all icons

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -2,22 +2,79 @@ import * as React from 'react'
 import classNames from 'classnames'
 import * as styles from './Icon.scss'
 import {
-  external,
-  favorite,
-  gear,
+  AddCircle,
+  ArrowDropDown,
+  ArrowDropUp,
+  CancelCircle,
+  ChartBar,
+  ChartBubble,
+  ChartLine,
+  ChartMulti,
+  ChartPie,
+  Close,
+  DotsHorz,
+  DotsVert,
+  DragHandle,
+  Edit,
+  ExpandMore,
+  ExploreCircle,
+  External,
+  Favorite,
+  FavoriteOutline,
+  File,
+  Filter,
+  Hamburger,
+  Note,
+  Redo,
+  Refresh,
+  RemoveCircle,
+  Search,
+  Settings,
+  TextFormat,
+  Trash,
+  TrashOutline,
+  Undo
 } from '../../icons/IconList'
 
 export const ICON_BUNDLE = {
-  external,
-  favorite,
-  gear
+  AddCircle,
+  ArrowDropDown,
+  ArrowDropUp,
+  CancelCircle,
+  ChartBar,
+  ChartBubble,
+  ChartLine,
+  ChartMulti,
+  ChartPie,
+  Close,
+  DotsHorz,
+  DotsVert,
+  DragHandle,
+  Edit,
+  ExpandMore,
+  ExploreCircle,
+  External,
+  Favorite,
+  FavoriteOutline,
+  File,
+  Filter,
+  Hamburger,
+  Note,
+  Redo,
+  Refresh,
+  RemoveCircle,
+  Search,
+  Settings,
+  TextFormat,
+  Trash,
+  TrashOutline,
+  Undo
 }
-
 
 export type IconSource = keyof typeof ICON_BUNDLE
 
 export interface IconProps {
-  name: IconSource,
+  glyph: IconSource,
   accessibilityLabel?: string
   color?: string
   className?: string
@@ -32,12 +89,12 @@ type PropertyBag = {
 
 */
 
-export const Icon: React.SFC<IconProps> = ({className, name,accessibilityLabel,...args}) => {
+export const Icon: React.SFC<IconProps> = ({className, glyph ,accessibilityLabel,...args}) => {
   const styleableProps: PropertyBag = {
 
   }
 
-  let icon = ICON_BUNDLE[name]
+  let icon = ICON_BUNDLE[glyph]
 
   return (
     <span className={classNames(styles.lensIcon, className, styleableProps)} {...args} dangerouslySetInnerHTML={{__html: icon}} aria-label={accessibilityLabel}></span>
