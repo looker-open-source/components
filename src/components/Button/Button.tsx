7d1969bf3ba3424ff8660612fc6d2b9a0fe6d7cd
Nathan Agrin <n8agrin@gmail.com>
2018-06-29 15:33:23 -0700

    Add button sizes

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,11 +3,49 @@ import sc, { StyledComponentClass } from 'styled-components'
 import { rem } from 'polished'
 import { brandFont } from '../../styles/typography'
 
-export interface ButtonProps {}
+export enum ButtonSizes {
+  ExtraSmall = "xsmall",
+  Small = "small",
+  Medium = "medium",
+  Large = "large"
+}
+
+export interface ButtonProps {
+  size: ButtonSizes
+}
+
+function buttonSize(size: string) {
+  switch (size) {
+    case ButtonSizes.ExtraSmall:
+      return {
+        fontSize: rem(12),
+        lineHeight: rem(20),
+        padding: `${rem(2)} ${rem(8)}`
+      }
+    case ButtonSizes.Small:
+      return {
+        fontSize: rem(14),
+        lineHeight: rem(24),
+        padding: `${rem(3)} ${rem(14)}`
+      }
+    case ButtonSizes.Large:
+      return {
+        fontSize: rem(22),
+        lineHeight: rem(30),
+        padding: `${rem(8)} ${rem(24)}`
+      }
+    default:
+      return {
+        fontSize: rem(16),
+        lineHeight: rem(24),
+        padding: `${rem(6)} ${rem(16)}`
+      }
+  }
+}
 
 export const Button = sc<ButtonProps, 'button'>('button')`
-  --background-color: ${(props) => props.theme.colors.action };
-  --color: ${(props) => props.theme.colors.text };
+  --background-color: ${props => props.theme.colors.action };
+  --color: ${props => props.theme.colors.text };
   --border-color: var(--background-color);
 
   background-color: var(--background-color);
@@ -17,9 +55,9 @@ export const Button = sc<ButtonProps, 'button'>('button')`
   cursor: pointer;
   display: inline-block;
   font-family: ${brandFont};
-  font-size: ${rem(16)};
-  line-height: ${rem(24)};
-  padding: ${rem(6)} ${rem(16)};
+  font-size: ${props => buttonSize(props.size).fontSize};
+  line-height: ${props => buttonSize(props.size).lineHeight};
+  padding: ${props => buttonSize(props.size).padding};
   white-space: nowrap;
   vertical-align: middle;
 
