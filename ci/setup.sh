f5aaf74f07ca32d5afe85b665b190dc349010388
Nathan Agrin <n8agrin@gmail.com>
2018-07-23 15:38:54 -0700

    Add commands to automatically generate .npmrc file
    Adds
    
    yarn npmrc > creates the npmrc file
    yarn npmrc:clean > removes the npmrc file
    bin/create_npmrc.sh > low level script to generate the npmrc file
    bin/get_nexus_creds.sh > low level script to extract nexus user/pwd from the gradle file used by Helltool
    ci/setup.sh > runs various commands to setup the local environment
    ci/test.sh > runs setup.sh and yarn test

--- ci/setup.sh
+++ ci/setup.sh
@@ -1,36 +1,4 @@
 #!/usr/bin/env bash
 
-case $1 in
-create_npmrc)
-  echo "CREATE NPMRC: Setup .npmrc"
-
-  : "${NEXUS_USER:?NEXUS_USER must not be empty}"
-  : "${NEXUS_PASS:?NEXUS_PASS must not be empty}"
-
-  AUTH=`echo -n "${NEXUS_USER}:${NEXUS_PASS}" | openssl base64`
-
-  NPMRC="registry=https://nexusrepo.looker.com/repository/looker-npm-group/
-always-auth=true
-_auth=${AUTH}"
-
-  echo "${NPMRC}" > '.npmrc'
-  ;;
-
-cleanup_npmrc)
-  echo "CLEANUP"
-  rm .npmrc
-  ;;
-
-*)
-    echo "Usage: setup-npmrc.sh SECTION"
-    echo
-    echo "CI Scripts"
-    echo
-    echo "SECTION               DESCRIPTION"
-    echo "-------               -----------"
-    echo "create_npmrc          Run code which prepares the source code before attempting build"
-    echo "cleanup_npmrc         Run code cleans up once build is complete"
-    exit 1
-    ;;
-
-esac
+yarn npmrc
+yarn install
