ae95d5f744b2946e33920c6b985de7a475f05be4
Jared Hardy <jared.hardy@looker.com>
2018-07-23 14:47:35 -0700

    Update label component

--- src/components/Form/Label/Label.tsx
+++ src/components/Form/Label/Label.tsx
@@ -8,10 +8,29 @@ export { ThemeInterface }
 import { space800 } from '../../../styles/colors'
 
 export interface LabelProps {
-  for: string
+  htmlFor: string
 }
 
-export const Label = styled<LabelProps, 'label'>('label')`
+/**
+ * Labels are used to descript an input field.
+ */
+const LabelGenerator: React.SFC<LabelProps> = ({ htmlFor, ...args }) => {
+  // // This prevents our props from being passed directly to the underlying tags, which ultimately
+  // // would cause some warnings. Ideally we would define the return type for this function, and
+  // // Typescript would warn us when passing props that are invalid.
+  // //
+  // See https://reactjs.org/warnings/unknown-prop.html
+  // const props = Object.assign({}, args)
+  // delete props.htmlFor
+
+  return (
+    <label htmlFor={htmlFor} {...args}>
+      {args.children}
+    </label>
+  )
+}
+
+export const Label = styled<LabelProps>(LabelGenerator)`
   font-weight: 600;
   color: ${space800};
   margin-right: 8px;
