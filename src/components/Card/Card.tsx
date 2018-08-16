ddb067600f9f3a08618e1a9b449792f4c44eadb0
Jared Hardy <jared.hardy@looker.com>
2018-08-16 08:45:13 -0700

    Updates to core elevation and transitions

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -13,16 +13,18 @@ export interface CardProps {
 export const Card = styled<CardProps, 'div'>('div')`
   background: #fff;
   border-radius: 4px;
-  box-shadow: ${props => (props.raised ? props.theme.shadows.small : 'none')};
+  box-shadow: ${props => (props.raised ? props.theme.shadows.one : 'none')};
   border: solid 1px ${props => props.theme.colors.borderColor};
   display: flex;
   flex-direction: column;
   height: 100%;
   min-width: 200px;
   overflow: hidden;
-  transition: border ${props => props.theme.transitions.durationShorter} ease;
+  transition: border ${props => props.theme.transitions.durationQuick} ease,
+    box-shadow ${props => props.theme.transitions.durationQuick} ease;
 
   &:hover {
     border-color: ${charcoal300};
+    box-shadow: ${props => (props.raised ? props.theme.shadows.two : 'none')};
   }
 `
--- src/themes/theme_transitions.ts
+++ src/themes/theme_transitions.ts
@@ -1,13 +1,18 @@
 export interface ThemeTransitions {
-  durationLong: number
-  durationShort: number
-  durationShorter: number
-  durationStandard: number
+  durationComplex: string
+  durationIntricate: string
+  durationModerate: string
+  durationQuick: string
+  durationRapid: string
+  durationSimple: string
 }
 
+// tslint:disable:object-literal-sort-keys
 export const themeTransitions = {
-  durationLong: 375,
-  durationShort: 200,
-  durationShorter: 150,
-  durationStandard: 300
+  durationRapid: '100ms',
+  durationQuick: '150ms',
+  durationSimple: '200ms',
+  durationModerate: '300ms',
+  durationComplex: '300ms',
+  durationIntricate: '500ms'
 }
