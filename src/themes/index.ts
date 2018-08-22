1097703e4842cf636592fc9b1bd65134f6d13e36
Nathan Agrin <n8agrin@gmail.com>
2018-08-22 13:27:48 -0700

    Add fontSizes, lineHeights, space for styled-system

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -1,3 +1,4 @@
+import { rem } from 'polished'
 import { themeColors, ThemeColors } from './theme_colors'
 import { ThemeEasings, themeEasings } from './theme_easings'
 import { themeShadows, ThemeShadows } from './theme_elevation'
@@ -17,6 +18,10 @@ export interface ThemeInterface {
   shadows: ThemeShadows
   spacing: ThemeSpacing
   transitions: ThemeTransitions
+
+  fontSizes: string[]
+  lineHeights: string[]
+  space: string[]
 }
 
 const theme = {
@@ -26,7 +31,13 @@ const theme = {
   lineHeightRamp: themeLineHeightRamp,
   shadows: themeShadows,
   spacing: themeSpacing,
-  transitions: themeTransitions
+  transitions: themeTransitions,
+
+  // styled-system theme properties
+  // tslint:disable-next-line:object-literal-sort-keys
+  fontSizes: [0, 12, 14, 16, 18, 22, 25, 36, 46, 58].map(rem),
+  lineHeights: [0, 16, 20, 24, 28, 32, 40, 52, 64, 84].map(rem),
+  space: [0, 4, 6, 8, 16, 20, 24, 30, 36, 46].map(rem)
 }
 
 export default theme
