1db0f6556ef9226b5e58afc1c7f6e526744bf82b
Nathan Agrin <n8agrin@gmail.com>
2018-08-31 16:51:19 -0700

    Add value and onChange props to FieldText

--- src/components/Form/Fields/FieldText.tsx
+++ src/components/Form/Fields/FieldText.tsx
@@ -7,13 +7,21 @@ export interface FieldTextProps {
   id: string
   label: string
   name: string
+  value?: string
+  onChange?: React.ChangeEventHandler<HTMLInputElement>
 }
 
-export const FieldText: React.SFC<FieldTextProps> = ({ id, label, name }) => {
+export const FieldText: React.SFC<FieldTextProps> = ({
+  id,
+  label,
+  name,
+  value,
+  onChange,
+}) => {
   return (
     <FormControl>
       <Label htmlFor={id}>{label}</Label>
-      <InputText id={id} name={name} />
+      <InputText id={id} name={name} value={value} onChange={onChange} />
     </FormControl>
   )
 }
