5c3b8d84ec448ac2757893c1921e160064beb9a8
Nathan Agrin <n8agrin@gmail.com>
2018-09-07 15:13:30 -0700

    Remove unused utils dir

--- src/utils/strings.ts
+++
@@ -1,7 +0,0 @@
-export function capitalize(str: string) {
-  return `${str.charAt(0).toUpperCase()}${str.slice(1)}`
-}
-
-export function decapitalize(str: string) {
-  return `${str.charAt(0).toLowerCase()}${str.slice(1)}`
-}
