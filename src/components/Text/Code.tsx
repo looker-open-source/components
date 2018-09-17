e8f74a050e6df970a9cdf765c624bb0cb1e5558c
Luke Bowerman <luke@looker.com>
2018-09-17 11:20:56 -0700

    Deprecate .extend method
    Leaving .extend usage within ResetComponent intact until @n8agrin is able to remove / refactor it.

--- src/components/Text/Code.tsx
+++ src/components/Text/Code.tsx
@@ -1,6 +1,7 @@
+import styled from '../../styled_components'
 import { codeFont } from '../../styles/typography'
 import { Text } from '../Text/Text'
 
-export const Code = Text.extend`
+export const Code = styled(Text)`
   font-family: ${codeFont};
 `.withComponent('code')
