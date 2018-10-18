3df93e183c3b2d90573e2df410e716129c7248b8
Scott Sirowy <scott.sirowy@looker.com>
2018-10-18 13:39:53 -0700

    Fixed isValidColor.

--- src/components/Colors/ColorWheel/color_utils.ts
+++ src/components/Colors/ColorWheel/color_utils.ts
@@ -3,5 +3,4 @@ import { rgb } from 'd3-color'
 /**
  * Utility to determine if color is valid or not.
  */
-export const isValidColor = (color?: string): boolean =>
-  color ? rgb(color).displayable() : false
+export const isValidColor = (color: string): boolean => rgb(color).displayable()
