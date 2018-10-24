8886e895f49f8a91dc151a6cdeb508ccb3a2df35
Melissa Boucher <melissaboucher@mmb.local>
2018-10-23 17:11:50 -0700

    adding CheckCircle, CheckProgress, Info icons

---
+++ src/icons/components/IconCheckCircle.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+import CheckCircle from '../glyphs/CheckCircle'
+export const IconCheckCircle: React.SFC<
+  React.SVGAttributes<SVGElement>
+> = props => <CheckCircle {...props} />
