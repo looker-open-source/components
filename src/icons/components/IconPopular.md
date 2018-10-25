82b0f0ff45ac5f3ed524f5c64f5854cf76b99057
Jared Hardy <jared.hardy@looker.com>
2018-10-25 13:13:38 -0700

    Add new icons

---
+++ src/icons/components/IconPopular.md
@@ -0,0 +1,5 @@
+# IconPopular
+
+```js
+<IconPopular />
+```
---
+++ src/icons/components/IconRecentActivity.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+import RecentActivity from '../glyphs/RecentActivity'
+export const IconRecentActivity: React.SFC<
+  React.SVGAttributes<SVGElement>
+> = props => <RecentActivity {...props} />
