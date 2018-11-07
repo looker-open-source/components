cb0912b70ff0923892095e2c74d004337b826102
Nathan Agrin <n8agrin@gmail.com>
2018-11-07 10:12:09 -0800

    Update Slider tests

--- src/components/Form/Inputs/Slider/Slider.test.tsx
+++ src/components/Form/Inputs/Slider/Slider.test.tsx
@@ -1,47 +1,33 @@
-import { mount } from 'enzyme'
 import 'jest-styled-components'
 import * as React from 'react'
-import { createWithTheme } from '../../../../../test/utils/create_with_theme'
+import { mountWithTheme } from '../../../../../test/utils/create_with_theme'
+import { assertSnapshot } from '../../../../../test/utils/snapshot'
 import { Slider } from './Slider'
 
 test('Slider default', () => {
-  const component = createWithTheme(<Slider />)
-  const tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
+  assertSnapshot(<Slider />)
 })
 
 test('Slider with min and max', () => {
-  const component = createWithTheme(
-    <Slider min={1} max={11} step={0.01} value={11} />
-  )
-  const tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
+  assertSnapshot(<Slider min={1} max={11} step={0.01} value={11} />)
 })
 
 test('Slider with name and id', () => {
-  const component = createWithTheme(<Slider name="Slip" id="Slide" />)
-  const tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
+  assertSnapshot(<Slider name="Slip" id="Slide" />)
 })
 
 test('Slider should accept disabled', () => {
-  const component = createWithTheme(<Slider disabled />)
-  const tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
+  assertSnapshot(<Slider disabled />)
 })
 
 test('Slider with aria-describedby', () => {
-  const component = createWithTheme(<Slider aria-describedby="some-id" />)
-  const tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
+  assertSnapshot(<Slider aria-describedby="some-id" />)
 })
 
 test('Should trigger onChange handler', () => {
   let counter = 0
   const handleChange = () => counter++
-
-  const wrapper = mount(<Slider onChange={handleChange} />)
-
+  const wrapper = mountWithTheme(<Slider onChange={handleChange} />)
   wrapper.find('input').simulate('change', { target: { value: '' } })
   expect(counter).toEqual(1)
 })
