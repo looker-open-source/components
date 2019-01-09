caac11d1d126cc849010960d14b7f54e2082bd62
thomas.pinella@looker.com <thomas.pinella@looker.com>
2019-01-09 15:54:59 -0800

    CA for tooltips, and Modal

--- styleguide_components/ThemeWrapper.tsx
+++ styleguide_components/ThemeWrapper.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import { ThemeProvider } from 'styled-components'
+import { CustomizableModalAttributes } from '../src/components/Modal/Modal'
 import { CustomizableOverlayAttributes } from '../src/components/Overlay/Overlay'
 import { theme } from '../src/style'
 
 CustomizableOverlayAttributes.zIndex = 100
-theme.components.Modal.zIndex = 100
+CustomizableModalAttributes.zIndex = 100
 
 export default class ThemeWrapper extends React.Component {
   public render() {
