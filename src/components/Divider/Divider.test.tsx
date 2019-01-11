7da0613387817738af96bf14a07a48102e6821f2
Jared Hardy <jared.hardy@looker.com>
2019-01-11 13:52:13 -0800

    Acutally add tests

---
+++ src/components/Divider/Divider.test.tsx
@@ -0,0 +1,16 @@
+import 'jest-styled-components'
+import * as React from 'react'
+import { assertSnapshot } from '../../../test/utils/snapshot'
+import { Divider } from './Divider'
+
+test('Default Divider', () => {
+  assertSnapshot(<Divider />)
+})
+
+test('Divider with a variant', () => {
+  assertSnapshot(<Divider variant="onDark" />)
+})
+
+test('A custom Divider', () => {
+  assertSnapshot(<Divider size="20px" customColor="turquoise" />)
+})
