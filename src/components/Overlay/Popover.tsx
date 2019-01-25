looker-open-source/components

commit d72a9c76840f42f7b5aaf96276f7befd6d7f5eea

CA for Popover, removed theme from label, validationmessage, overlay

src/components/Overlay/Popover.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react'
-import { withTheme } from '../../style'
-import { ThemedProps } from '../../types'
-import { Overlay, OverlayInteractiveProps } from './Overlay'
+import { shadows } from '../../style'
+import { CustomizableAttributes } from '../../types/attributes'
+import { BackdropStyle, Overlay, OverlayInteractiveProps } from './Overlay'
+import { OverlayBubbleStyleProps } from './OverlayBubble'
 import { overlayBubbleWithContent } from './popover_utils'
 
 export interface PopoverProps extends OverlayInteractiveProps {
   content: React.ReactNode
 }
 
-const InternalPopover: React.SFC<ThemedProps<PopoverProps>> = ({
+export const Popover: React.SFC<PopoverProps> = ({
   content,
-  theme,
   children,
   ...overlayProps
 }) => (
@@ -18,13 +18,28 @@ const InternalPopover: React.SFC<ThemedProps<PopoverProps>> = ({
     trigger="click"
     overlayContentFactory={overlayBubbleWithContent(
       content,
-      theme.components.Popover.bubble
+      CustomizablePopoverAttributes.bubble
     )}
-    backdropStyles={theme.components.Popover.backdrop}
+    backdropStyles={CustomizablePopoverAttributes.backdrop}
     {...overlayProps}
   >
     {children}
   </Overlay>
 )
 
-export const Popover = withTheme(InternalPopover)
+export interface CustomizablePopoverAttributes extends CustomizableAttributes {
+  backdrop: BackdropStyle
+  bubble: OverlayBubbleStyleProps
+}
+
+export const CustomizablePopoverAttributes: CustomizablePopoverAttributes = {
+  backdrop: {},
+  bubble: {
+    backgroundColor: 'palette.white',
+    border: '1px solid',
+    borderColor: 'palette.charcoal200',
+    borderRadius: 'medium',
+    boxShadow: shadows[3],
+    color: 'palette.charcoal900',
+  },
+}

src/components/Form/Label/Label.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react'
-import { Theme } from '../../../style'
 import { fontWeights } from '../../../style/font_weights'
 import { CustomizableAttributes } from '../../../types/attributes'
 import { Box, BoxProps } from '../../Box'
 
 export interface LabelProps extends BoxProps<HTMLLabelElement> {
   htmlFor?: string
-  theme?: Theme
 }
 
 export const Label: React.SFC<LabelProps> = ({ ...props }) => (

src/components/Form/ValidationMessage/ValidationMessage.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { Theme } from '../../../style'
 import { CustomizableAttributes } from '../../../types/attributes'
 import { Box, BoxProps } from '../../Box'
 
@@ -14,7 +13,6 @@ export interface ValidationMessageProps extends BoxProps<HTMLDivElement> {
    * The validation message to render.
    */
   message?: string
-  theme?: Theme
 }
 
 export const ValidationMessage: React.SFC<ValidationMessageProps> = ({

src/components/Modal/ModalBackdrop.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import { Styles } from 'styled-components'
 import { styled, Theme, withTheme } from '../../style'
 import { Box, BoxProps } from '../Box'
+import { CustomizableOverlayAttributes } from '../Overlay'
 
 export interface BackdropProps {
   close: () => void
@@ -10,18 +11,13 @@ export interface BackdropProps {
   className?: string
 }
 
-const Internal: React.SFC<BackdropProps> = ({
-  close,
-  style,
-  theme,
-  className,
-}) => (
+const Internal: React.SFC<BackdropProps> = ({ close, style, className }) => (
   <Backdrop
     className={className}
-    bg={theme.components.Overlay.backdrop.backgroundColor}
+    bg={CustomizableOverlayAttributes.backdrop.backgroundColor}
     height="100%"
     onClick={close}
-    opacity={theme.components.Overlay.backdrop.opacity}
+    opacity={CustomizableOverlayAttributes.backdrop.opacity}
     position="fixed"
     // style={{ cursor: 'pointer', ...style }}
     backdropStyle={style}
@@ -52,6 +48,6 @@ const Backdrop = styled(BackdropFactory)<BackdropInternalProps>`
   }
 
   &.exited {
-    opacity: ${props => props.theme.components.Overlay.backdrop.opacity};
+    opacity: ${CustomizableOverlayAttributes.backdrop.opacity};
   }
 `

src/components/Overlay/Overlay.tsx
@@ -7,7 +7,6 @@ import {
   Reference,
   RefHandler,
 } from 'react-popper'
-import { Theme } from '../../style'
 import { CustomizableAttributes } from '../../types/attributes'
 import { Box } from '../Box'
 
@@ -56,11 +55,6 @@ export interface OverlayProps extends OverlayInteractiveProps {
    */
   overlayContentFactory: (props: OverlayContentProps) => React.ReactNode
   /**
-   * A Lens compatible theme object. This is passed in automatically by the
-   * withTheme higher-order helper.
-   */
-  theme: Theme
-  /**
    * Optional backdrop styles to merge with the Backdrop implementation. These
    * must be a CSSProperty compatible key / value paired object. For example
    * {backgroundColor: 'pink'}.
@@ -90,7 +84,6 @@ export class Overlay extends React.Component<OverlayProps, OverlayState> {
   public static defaultProps: OverlayProps = {
     open: false,
     overlayContentFactory: () => null,
-    theme: {} as Theme,
     trigger: 'hover',
   }
   private popperRef: HTMLElement | null
@@ -244,12 +237,14 @@ export class Overlay extends React.Component<OverlayProps, OverlayState> {
   }
 }
 
+export interface BackdropStyle {
+  backgroundColor?: string
+  opacity?: number
+}
+
 export interface CustomizableOverlayAttributes extends CustomizableAttributes {
   zIndex: number
-  backdrop: {
-    backgroundColor: string
-    opacity: number
-  }
+  backdrop: BackdropStyle
 }
 
 export const CustomizableOverlayAttributes: CustomizableOverlayAttributes = {

src/style/components.ts
@@ -8,20 +8,6 @@ export interface Components {
     zIndex: number
     surface: OverlayBubbleStyleProps
   }
-  Overlay: {
-    zIndex: number
-    backdrop: {
-      backgroundColor: string
-      opacity: number
-    }
-  }
-  Popover: {
-    backdrop: {
-      backgroundColor?: string
-      opacity?: number
-    }
-    bubble: OverlayBubbleStyleProps
-  }
   RichTooltip: {
     bubble: OverlayBubbleStyleProps
   }
@@ -41,21 +27,6 @@ export const components: Components = {
       color: palette.charcoal000,
     },
     zIndex: 0,
-  },
-  Overlay: {
-    backdrop: { backgroundColor: palette.charcoal200, opacity: 0.6 },
-    zIndex: 0,
-  },
-  Popover: {
-    backdrop: {},
-    bubble: {
-      backgroundColor: palette.white,
-      border: '1px solid',
-      borderColor: palette.charcoal200,
-      borderRadius: radii.medium,
-      boxShadow: shadows[3],
-      color: palette.charcoal900,
-    },
   },
   RichTooltip: {
     bubble: {

styleguide_components/ThemeWrapper.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react'
 import { ThemeProvider } from 'styled-components'
+import { CustomizableOverlayAttributes } from '../src/components/Overlay/Overlay'
 import { theme } from '../src/style'
 
-theme.components.Overlay.zIndex = 100
+CustomizableOverlayAttributes.zIndex = 100
 theme.components.Modal.zIndex = 100
 
 export default class ThemeWrapper extends React.Component {

commit a0a20abe8d5dee50202f934b5170fd3f26b478c4

Restore Popover arrow
Fixing a regression introduced from `d72a9c76`

src/components/Overlay/Popover.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { fadeIn, shadows } from '../../style'
+import { fadeIn, palette, shadows } from '../../style'
 import { CustomizableAttributes } from '../../types/attributes'
 import { BackdropStyle, Overlay, OverlayInteractiveProps } from './Overlay'
 import { OverlayBubbleStyleProps } from './OverlayBubble'
@@ -36,11 +36,11 @@ export const CustomizablePopoverAttributes: CustomizablePopoverAttributes = {
   backdrop: {},
   bubble: {
     animation: `${fadeIn} 0.2s linear`,
-    backgroundColor: 'palette.white',
+    backgroundColor: palette.white,
     border: '1px solid',
-    borderColor: 'palette.charcoal200',
+    borderColor: palette.charcoal200,
     borderRadius: 'medium',
     boxShadow: shadows[3],
-    color: 'palette.charcoal900',
+    color: palette.charcoal900,
   },
 }