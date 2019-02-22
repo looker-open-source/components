76cb0330edc1a363f208ad852122a90cc22439cd
Jared Hardy <jared.hardy@looker.com>
2019-02-22 15:27:52 -0800

    update tests

--- src/components/Menu/Menu.test.tsx
+++ src/components/Menu/Menu.test.tsx
@@ -2,6 +2,7 @@ import { shallow } from 'enzyme'
 import 'jest-styled-components'
 import * as React from 'react'
 
+import { palette } from '../../style'
 import { Menu } from './Menu'
 import { MenuGroup } from './MenuGroup'
 import { MenuItem } from './MenuItem'
@@ -66,6 +67,61 @@ test('Menu - canActivate composition', () => {
         <MenuItem>Swiss</MenuItem>
       </MenuGroup>
       <MenuGroup canActivate={false}>
+        <MenuItem icon="Beaker">Scary Stuff</MenuItem>
+      </MenuGroup>
+    </Menu>
+  )
+
+  expect(menu).toMatchSnapshot()
+})
+
+test('Menu - menu customization', () => {
+  const menuCustomizations =
+    // tslint:disable:object-literal-sort-keys
+    {
+      bg: palette.purple500,
+      color: palette.purple200,
+      icon: {
+        color: palette.purple300,
+        size: 20,
+      },
+      marker: {
+        color: palette.purple900,
+        size: 10,
+      },
+      hover: {
+        bg: palette.purple700,
+        color: palette.white,
+        icon: {
+          color: palette.purple100,
+        },
+      },
+      current: {
+        bg: palette.purple200,
+        color: palette.purple900,
+        icon: {
+          color: palette.purple500,
+        },
+      },
+      activated: {
+        color: palette.blue500,
+      },
+      // tslint:disable:enable-literal-sort-keys
+    }
+
+  const menu = shallow(
+    <Menu customizationProps={menuCustomizations}>
+      <MenuGroup>
+        <MenuItem icon="LogoRings">Looker</MenuItem>
+        <MenuItem icon="Validate">Validate</MenuItem>
+        <MenuItem icon="ChartPie">Pizza!</MenuItem>
+      </MenuGroup>
+      <MenuGroup label="Cheeses">
+        <MenuItem>Gouda</MenuItem>
+        <MenuItem>Cheddar</MenuItem>
+        <MenuItem>Swiss</MenuItem>
+      </MenuGroup>
+      <MenuGroup>
         <MenuItem icon="Beaker">Scary Stuff</MenuItem>
       </MenuGroup>
     </Menu>
--- src/components/Menu/MenuGroup.test.tsx
+++ src/components/Menu/MenuGroup.test.tsx
@@ -2,6 +2,7 @@ import 'jest-styled-components'
 import * as React from 'react'
 import { assertSnapshot } from '../../../test/utils/snapshot'
 
+import { palette } from '../../style'
 import { Box } from '../Box'
 import { MenuGroup } from './MenuGroup'
 import { MenuItem } from './MenuItem'
@@ -56,4 +57,49 @@ test('MenuGroup - labelProps & labelStyles', () => {
       <MenuItem>where?</MenuItem>
     </MenuGroup>
   )
+})
+
+test('MenuGrouo - menu customization', () => {
+  const menuCustomizations =
+    // tslint:disable:object-literal-sort-keys
+    {
+      bg: palette.purple500,
+      color: palette.purple200,
+      icon: {
+        color: palette.purple300,
+        size: 20,
+      },
+      marker: {
+        color: palette.purple900,
+        size: 10,
+      },
+      hover: {
+        bg: palette.purple700,
+        color: palette.white,
+        icon: {
+          color: palette.purple100,
+        },
+      },
+      current: {
+        bg: palette.purple200,
+        color: palette.purple900,
+        icon: {
+          color: palette.purple500,
+        },
+      },
+      activated: {
+        color: palette.blue500,
+      },
+      // tslint:disable:enable-literal-sort-keys
+    }
+
+  const menu = assertSnapshot(
+    <MenuGroup customizationProps={menuCustomizations}>
+      <MenuItem icon="LogoRings">Looker</MenuItem>
+      <MenuItem icon="Validate">Validate</MenuItem>
+      <MenuItem icon="ChartPie">Pizza!</MenuItem>
+    </MenuGroup>
+  )
+
+  expect(menu).toMatchSnapshot()
 })
