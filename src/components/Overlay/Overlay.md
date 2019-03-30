3bdfa79dd48b7c8e8ad8fa9d3c3ffc3dde0a7444
Luke Bowerman <luke@looker.com>
2019-03-29 20:57:15 -0700

    [LENS-119] Test for tooltip positioning within Overlay

--- src/components/Overlay/Overlay.md
+++ src/components/Overlay/Overlay.md
@@ -1,2 +1,7 @@
-
 Overlay is the abstract building block of Popover and RichTooltip.
+
+```js
+const PopoverOverlayDemo = require('../../../styleguide_components/PopoverOverlayDemo')
+  .PopoverOverlayDemo
+;<PopoverOverlayDemo />
+```
--- src/components/Overlay/Tooltip.test.tsx
+++ src/components/Overlay/Tooltip.test.tsx
@@ -3,11 +3,13 @@ import 'jest-styled-components'
 import * as React from 'react'
 import { mountWithTheme } from '../../../test/utils/create_with_theme'
 import { assertSnapshot } from '../../../test/utils/snapshot'
+import { Box } from '../Box'
 import { Button } from '../Button'
 import { Paragraph } from '../Text'
 import { mouseEventSimulator } from './overlay.test.helpers'
 import { OverlayHover } from './OverlayHover'
 import { OverlaySurface } from './OverlaySurface'
+import { Popover } from './Popover'
 import { Tooltip } from './Tooltip'
 
 describe('Tooltip', () => {
@@ -125,5 +127,30 @@ describe('Tooltip', () => {
     const paragraph = surface.find(Paragraph)
     expect(paragraph.props().maxWidth).toEqual('20rem')
     expect(paragraph.props().textAlign).toEqual('right')
+  })
+
+  // Proves LENS-119 is invalid
+  test('tooltip can exceed bounds of containing overlay', () => {
+    const tooltip = (
+      <Tooltip content="Great knowledge here!" isOpen>
+        {(eventHandlers, ref) => (
+          <Box innerRef={ref} {...eventHandlers}>
+            I wish I knew more about this...
+          </Box>
+        )}
+      </Tooltip>
+    )
+
+    const popover = (
+      <Popover isOpen content={tooltip}>
+        {(onClick, ref) => (
+          <Button onClick={onClick} innerRef={ref}>
+            Test
+          </Button>
+        )}
+      </Popover>
+    )
+
+    assertSnapshot(popover)
   })
 })
