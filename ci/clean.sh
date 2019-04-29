bd1d7cade13e6e95eab233fce25a21eca6ed7b7f
Luke Bowerman <luke@looker.com>
2019-04-29 15:41:34 -0700

    Use ci/*.sh CI configuration pattern
    Clean-up package.json scripts to be clearer
    Use ci/clean.sh for clean

---
+++ ci/clean.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+rm -rf dist
+rm -rf styleguide
+rm -rf src/icons/build
+rm -rf lens-styleguide.tar.gz
--- ci/test.sh
+++ ci/test.sh
@@ -1,8 +1,2 @@
 #!/usr/bin/env bash
-
-cd $(dirname $0)/..
-WORKING_DIR=$PWD
-set -e
-
-ci/setup.sh
 yarn test
