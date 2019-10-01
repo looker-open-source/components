2bd1386be00fbe5c120bed5c1fe538bc54ae5967
Luke Bowerman <luke@looker.com>
2019-10-01 11:39:47 -0700

    ComponentDetail component

--- packages/www/src/components/index.ts
+++ packages/www/src/components/index.ts
@@ -1,3 +1,9 @@
+export { default as Intro } from './Intro'
+
+export { default as ComponentDetail } from './ComponentDetail'
+export { default as StatusAndResources } from './StatusAndResources'
+
+// Demos
 export { default as BorderRender } from './BorderRender'
 export { default as BoxSpacingRecipeTable } from './BoxSpacingRecipeTable'
 export { default as BreakpointTable } from './BreakpointTable'
@@ -5,8 +11,6 @@ export { default as ColorFieldPickerDemo } from './ColorFieldPickerDemo'
 export * from './ColorPalette'
 export { default as ColorWheelDemo } from './ColorWheelDemo'
 export { default as FormValidationDemo } from './FormValidationDemo'
-export { default as Intro } from './Intro'
 export { default as MenuDemo } from './MenuDemo'
 export { default as RichTooltipDemo } from './RichTooltipDemo'
 export { default as SpacingOptionsTable } from './SpacingOptionsTable'
-export { default as StatusAndResources } from './StatusAndResources'
