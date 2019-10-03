2cbcc5d2d2e9effa4b7fe53db715067644095ab5
Meredith Dodge <meredith.dodge@looker.com>
2019-10-03 16:55:35 -0700

    Convert Checkbox.tsx, simplify InputProps

--- packages/components/src/Form/Inputs/Checkbox/Checkbox.tsx
+++ packages/components/src/Form/Inputs/Checkbox/Checkbox.tsx
@@ -1,31 +1,10 @@
-import omit from 'lodash/omit'
-import React, { FunctionComponent, Ref } from 'react'
-import styled, { StyledComponent } from 'styled-components'
-import { Box, BoxProps } from '../../../Layout/Box'
+import styled from 'styled-components'
+import { space, SpaceProps } from '@looker/design-tokens'
 import { InputProps } from '../InputProps'
 
-export interface CheckboxProps
-  extends Omit<BoxProps<HTMLInputElement>, 'as'>,
-    InputProps {
-  /**
-   * Determines if the checkbox is checked or not.
-   */
-  checked?: boolean
-}
-
-type ComponentType = FunctionComponent<CheckboxProps>
-type StyledComponentType = StyledComponent<ComponentType, CheckboxProps>
-
-const InternalCheckbox: ComponentType = props => {
-  const type = { type: 'checkbox' }
-  return <Box as="input" {...omit(props, ['validationType'])} {...type} />
-}
-
-const CheckboxFactory = React.forwardRef<StyledComponentType, CheckboxProps>(
-  (props: CheckboxProps, ref: Ref<StyledComponentType>) => (
-    <InternalCheckbox ref={ref} {...props} />
-  )
-)
+export interface CheckboxProps extends SpaceProps, Omit<InputProps, 'type'> {}
 
 /** @component */
-export const Checkbox = styled<ComponentType>(CheckboxFactory)``
+export const Checkbox = styled.input.attrs({ type: 'checkbox' })<CheckboxProps>`
+  ${space}
+`
--- packages/components/src/Form/Inputs/InputProps.ts
+++ packages/components/src/Form/Inputs/InputProps.ts
@@ -1,35 +1,6 @@
+import { CompatibleHTMLProps } from '@looker/design-tokens'
 import { ValidationType } from '../ValidationMessage'
 
-export interface InputProps {
-  /**
-   * Specifies the unique id of an input.
-   */
-  id?: string
-  /**
-   * Specifies the name of an input, which is used in the context of a form in order to capture the value of the input. Also required for validation.
-   */
-  name?: string
-  /**
-   * Determines if an input is disabled.
-   * @default false
-   */
-  disabled?: boolean
-  /**
-   * Used to provide a detailed description of an input that contains errors.
-   */
-  'aria-describedby'?: string
-  /**
-   * Determines if the input is readonly.
-   * @default false
-   */
-  readOnly?: boolean
-  /**
-   * Determines if an input is required.
-   * @default false
-   */
-  required?: boolean
-  /**
-   * Specifies the type of validation -- error, warning, etc.
-   */
+export interface InputProps extends CompatibleHTMLProps<HTMLInputElement> {
   validationType?: ValidationType
 }
