402e159bf94db3243256878c46bc541691547be9
Meredith Dodge <meredith.dodge@looker.com>
2019-10-04 12:17:52 -0700

    Update Radio.tsx

--- packages/components/src/Form/Inputs/Checkbox/Checkbox.tsx
+++ packages/components/src/Form/Inputs/Checkbox/Checkbox.tsx
@@ -4,7 +4,6 @@ import { InputProps } from '../InputProps'
 
 export interface CheckboxProps extends SpaceProps, Omit<InputProps, 'type'> {}
 
-/** @component */
 export const Checkbox = styled.input.attrs({ type: 'checkbox' })<CheckboxProps>`
   ${space}
 `
--- packages/components/src/Form/Inputs/Radio/Radio.tsx
+++ packages/components/src/Form/Inputs/Radio/Radio.tsx
@@ -1,31 +1,9 @@
-import omit from 'lodash/omit'
-import React, { FunctionComponent, Ref } from 'react'
-import styled, { StyledComponent } from 'styled-components'
-import { Box, BoxProps } from '../../../Layout/Box'
+import styled from 'styled-components'
+import { space, SpaceProps } from '@looker/design-tokens'
 import { InputProps } from '../InputProps'
 
-export interface RadioProps
-  extends Omit<BoxProps<HTMLInputElement>, 'as'>,
-    InputProps {
-  /**
-   * Determines if the radio button is selected or not.
-   */
-  checked?: boolean
-}
+export interface RadioProps extends SpaceProps, Omit<InputProps, 'type'> {}
 
-type ComponentType = FunctionComponent<RadioProps>
-type StyledComponentType = StyledComponent<ComponentType, RadioProps>
-
-const InternalRadio: ComponentType = props => {
-  const type = { type: 'radio' }
-  return <Box as="input" {...omit(props, ['validationType'])} {...type} />
-}
-
-const RadioFactory = React.forwardRef<StyledComponentType, RadioProps>(
-  (props: RadioProps, ref: Ref<StyledComponentType>) => (
-    <InternalRadio ref={ref} {...props} />
-  )
-)
-
-/** @component */
-export const Radio = styled<ComponentType>(RadioFactory)``
+export const Checkbox = styled.input.attrs({ type: 'radio' })<RadioProps>`
+  ${space}
+`
