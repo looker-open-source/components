9ed7656f5bdc4712f832f6c8f6abe48c51340009
Meredith Dodge <meredith.dodge@looker.com>
2019-10-07 12:34:38 -0700

    Convert FormControl.tsx

--- packages/components/src/Form/FormControl/FormControl.tsx
+++ packages/components/src/Form/FormControl/FormControl.tsx
@@ -1,54 +1,38 @@
-import * as CSS from 'csstype'
-import React from 'react'
-import { ResponsiveValue } from 'styled-system'
-import { BoxProps } from '../../Layout/Box'
-import { Flex } from '../../Layout/Flex'
-
-type ResponsiveAlignItemsValue = ResponsiveValue<CSS.AlignItemsProperty>
-type ResponsiveFlexDirectionValue = ResponsiveValue<CSS.FlexDirectionProperty>
-type ResponsiveJustifyContentValue = ResponsiveValue<CSS.JustifyContentProperty>
+import styled, { css } from 'styled-components'
+import { Flex, FlexProps } from '../../Layout/Flex'
 
 type FormControlDirections = 'left' | 'right' | 'top' | 'bottom'
 
-export interface FormControlProps extends Omit<BoxProps<HTMLDivElement>, 'as'> {
+export interface FormControlProps extends FlexProps {
   alignLabel?: FormControlDirections
 }
 
-export const FormControl: React.FC<FormControlProps> = ({
-  alignLabel,
-  ...props
-}) => {
-  interface FlexAlignment {
-    alignItems?: ResponsiveAlignItemsValue
-    flexDirection?: ResponsiveFlexDirectionValue
-    justifyContent?: ResponsiveJustifyContentValue
+const setFlexAlignment = ({ alignLabel }: FormControlProps) => {
+  switch (alignLabel) {
+    case 'left':
+      return css`
+        flex-direction: row;
+        align-items: baseline;
+      `
+    case 'right':
+      return css`
+        flex-direction: row-reverse;
+        justify-content: flex-end;
+        align-items: baseline;
+      `
+    case 'bottom':
+      return css`
+        flex-direction: column-reverse;
+        justify-content: flex-end;
+      `
+    case 'top':
+    default:
+      return css`
+        flex-direction: column;
+      `
   }
-  const flexAlignment: FlexAlignment = {}
-  const setFlexAlignment = () => {
-    switch (alignLabel) {
-      case 'left':
-        flexAlignment.flexDirection = 'row'
-        flexAlignment.alignItems = 'baseline'
-        break
-      case 'right':
-        flexAlignment.flexDirection = 'row-reverse'
-        flexAlignment.justifyContent = 'flex-end'
-        flexAlignment.alignItems = 'baseline'
-        break
-      case 'bottom':
-        flexAlignment.flexDirection = 'column-reverse'
-        flexAlignment.justifyContent = 'flex-end'
-        break
-      case 'top':
-      default:
-        flexAlignment.flexDirection = 'column'
-        break
-    }
-    return flexAlignment
-  }
-  return (
-    <Flex {...setFlexAlignment()} {...props}>
-      {props.children}
-    </Flex>
-  )
 }
+
+export const FormControl = styled(Flex)<FormControlProps>`
+  ${setFlexAlignment}
+`
