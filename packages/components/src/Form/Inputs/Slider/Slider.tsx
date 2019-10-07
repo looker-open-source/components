d743d74a394a3fc479f7554ab627f03cde5e6931
Meredith Dodge <meredith.dodge@looker.com>
2019-10-06 21:28:44 -0700

    Remove Slider commented props

--- packages/components/src/Form/Inputs/Slider/Slider.tsx
+++ packages/components/src/Form/Inputs/Slider/Slider.tsx
@@ -2,24 +2,7 @@ import styled from 'styled-components'
 import { space, SpaceProps } from '@looker/design-tokens'
 import { InputProps } from '../InputProps'
 
-export interface SliderProps extends SpaceProps, Omit<InputProps, 'type'> {
-  // /**
-  //  * Minimum value for Slider.
-  //  */
-  // min?: number
-  // /**
-  //  * Maximum value for Slider.
-  //  */
-  // max?: number
-  // /**
-  //  * Granularity between values.
-  //  */
-  // step?: number
-  // /**
-  //  * Initial value.
-  //  */
-  // value?: number
-}
+export interface SliderProps extends SpaceProps, Omit<InputProps, 'type'> {}
 
 export const Slider = styled.input.attrs({ type: 'range' })<SliderProps>`
   ${space}
