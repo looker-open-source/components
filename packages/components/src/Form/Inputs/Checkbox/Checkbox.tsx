586feca1919a3add5da5c6d3ecc8bfc2fc973849
Meredith Dodge <meredith.dodge@looker.com>
2019-10-08 13:28:13 -0700

    Review & update Checkbox snapshots

--- packages/components/src/Form/Inputs/Checkbox/Checkbox.tsx
+++ packages/components/src/Form/Inputs/Checkbox/Checkbox.tsx
@@ -1,9 +1,10 @@
 import styled from 'styled-components'
-import { space, SpaceProps } from '@looker/design-tokens'
+import { reset, space, SpaceProps } from '@looker/design-tokens'
 import { InputProps } from '../InputProps'
 
 export interface CheckboxProps extends SpaceProps, Omit<InputProps, 'type'> {}
 
 export const Checkbox = styled.input.attrs({ type: 'checkbox' })<CheckboxProps>`
+  ${reset}
   ${space}
 `
