56544efc2ac71b07e64d151585436fff56176500
Meredith Dodge <meredith.dodge@looker.com>
2019-10-08 16:32:50 -0700

    Review and update Slider tests

--- packages/components/src/Form/Inputs/Slider/Slider.tsx
+++ packages/components/src/Form/Inputs/Slider/Slider.tsx
@@ -1,9 +1,10 @@
 import styled from 'styled-components'
-import { space, SpaceProps } from '@looker/design-tokens'
+import { reset, space, SpaceProps } from '@looker/design-tokens'
 import { InputProps } from '../InputProps'
 
 export interface SliderProps extends SpaceProps, Omit<InputProps, 'type'> {}
 
 export const Slider = styled.input.attrs({ type: 'range' })<SliderProps>`
+  ${reset}
   ${space}
 `
