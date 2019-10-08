54dcf230585b9a4a4fe109a0f0840a163d36c4a7
Meredith Dodge <meredith.dodge@looker.com>
2019-10-08 13:45:54 -0700

    Review and update Radio snapshots

--- packages/components/src/Form/Inputs/Radio/Radio.tsx
+++ packages/components/src/Form/Inputs/Radio/Radio.tsx
@@ -1,9 +1,10 @@
 import styled from 'styled-components'
-import { space, SpaceProps } from '@looker/design-tokens'
+import { reset, space, SpaceProps } from '@looker/design-tokens'
 import { InputProps } from '../InputProps'
 
 export interface RadioProps extends SpaceProps, Omit<InputProps, 'type'> {}
 
 export const Radio = styled.input.attrs({ type: 'radio' })<RadioProps>`
+  ${reset}
   ${space}
 `
