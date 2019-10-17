2dcdb48a933e9e8208705903c25c9abcf75d29b8
Luke Bowerman <luke@looker.com>
2019-10-17 16:34:43 -0700

    Fix Code font family config (and dictionary additions)

--- packages/design-tokens/src/tokens/typography/font_families.ts
+++ packages/design-tokens/src/tokens/typography/font_families.ts
@@ -2,5 +2,5 @@ import { FontFamilyChoices } from '../../system'
 
 export const fontFamilies: FontFamilyChoices = {
   brand: `'Open Sans', Helvetica, Arial, sans-serif`,
-  code: `'Monaco', 'Menlo', 'Ubuntu Mono', 'Consolas', 'source-code-pro', monospace`,
+  code: `Consolas, "Liberation Mono", Menlo, monospace`,
 }
