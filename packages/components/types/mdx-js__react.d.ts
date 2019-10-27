a3771082135d3f636421b363dcf81ce51695e15a
Luke Bowerman <luke@looker.com>
2019-10-26 17:14:06 -0700

    More React import clean-ups

--- packages/components/types/mdx-js__react.d.ts
+++ packages/components/types/mdx-js__react.d.ts
@@ -1,5 +1,5 @@
 declare module '@mdx-js/react' {
-  import React, { ComponentType, ReactNode } from 'react'
+  import * as React from 'react'
   type ComponentType =
     | 'p'
     | 'h1'
@@ -27,11 +27,11 @@ declare module '@mdx-js/react' {
     | 'img'
 
   export type Components = {
-    [key in ComponentType]?: ComponentType<any>
+    [key in ComponentType]?: React.ComponentType<any>
   }
   export interface MDXProviderProps {
-    children?: ReactNode
+    children?: React.ReactNode
     components: Components
   }
-  export class MDXProvider extends Component<MDXProviderProps> {}
+  export class MDXProvider extends React.Component<MDXProviderProps> {}
 }
