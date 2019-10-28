1a5baa2afd23e68848115c5674633a7c647ca98e
Luke Bowerman <luke@looker.com>
2019-10-28 15:31:44 -0700

    CI Setup for Gatsby

--- ci/clean.sh
+++ ci/clean.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
-rm -rf dist
-rm -rf styleguide
-rm -rf src/icons/build
-rm -rf lens-styleguide.tar.gz
+rm -rf packages/*/dist
+rm -rf packages/www/build
--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-cd styleguide
+yarn workspace www build
+cd packages/www/public
 aws s3 sync . s3://lens.looker.com --delete
