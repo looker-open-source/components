af766eca263b6fa6a4cb832e3859610c6fa86693
Lara Harrow <23616264+laraharrow@users.noreply.github.com>
2019-10-29 12:47:58 -0700

    add license info to files (#797)
    * add license info to *.tsx *.ts *.js files
    
    * Remove duplicate license entry
    
    * Add MIT license to shell scripts

--- __mocks__/popper.js
+++ __mocks__/popper.js
@@ -3,17 +3,17 @@
  MIT License
 
  Copyright (c) 2019 Looker Data Sciences, Inc.
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in all
  copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,25 +24,18 @@
 
  */
 
-import {
-  reset,
-  CompatibleHTMLProps,
-  border,
-  BorderProps,
-  typography,
-  TypographyProps,
-} from 'looker-design-tokens'
-import { css } from 'styled-components'
+// c.f. https://github.com/FezVrasta/popper.js#how-to-use-popperjs-in-jest
 
-export interface TableSectionProps
-  extends BorderProps,
-    TypographyProps,
-    CompatibleHTMLProps<HTMLTableSectionElement> {}
+/* eslint disable */
+import PopperJs from 'popper.js'
 
-export const tableSectionCSS = css`
-  ${reset}
-  ${border}
-  ${typography}
-`
+export default class Popper {
+  static placements = PopperJs.placements
 
-export const tableSectionDefaults: TableSectionProps = { textAlign: 'left' }
+  constructor() {
+    return {
+      destroy: () => {},
+      scheduleUpdate: () => {},
+    }
+  }
+}
