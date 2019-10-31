90299daec022fc065f02e97ddfa6edd6dfd12aa1
mdodgelooker <53451193+mdodgelooker@users.noreply.github.com>
2019-10-31 08:34:51 -0700

    FieldColor: id, ref, validationMessage (#807)
    * Add id, validationMessage, update prop spreading, use forwardRef on FieldColor

--- packages/components/src/Form/Fields/FieldColor/FieldColor.tsx
+++ packages/components/src/Form/Fields/FieldColor/FieldColor.tsx
@@ -3,17 +3,17 @@
  MIT License
 
  Copyright (c) 2019 Looker Data Sciences, Inc.
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in all
  copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,10 +24,12 @@
 
  */
 
-import React, { useState, ChangeEvent, FC, FormEvent } from 'react'
+import React, { useState, ChangeEvent, FormEvent, forwardRef, Ref } from 'react'
+import styled from 'styled-components'
+import uuid from 'uuid/v4'
 import { Popover, PopoverContent } from '../../../Popover'
 import { InputText, InputTextProps } from '../../Inputs/InputText'
-import { Field, FieldProps } from '../Field'
+import { Field, FieldProps, omitFieldProps, pickFieldProps } from '../Field'
 import { FormControl } from '../../FormControl'
 import { useFormContext } from '../../Form'
 import {
@@ -72,103 +74,118 @@ const createEventWithHSVValue = (
   } as ChangeEvent<HTMLInputElement>
 }
 
-export const FieldColor: FC<FieldColorProps> = ({
-  alignValidationMessage,
-  hideInput,
-  ...props
-}) => {
-  const validationMessage = useFormContext(props)
-  const initialValue =
-    props.value && isValidColor(props.value)
-      ? str2simpleHsv(props.value)
-      : polarbrightness2hsv(white())
+export const FieldColorComponent = forwardRef(
+  (
+    {
+      alignValidationMessage,
+      hideInput,
+      id = uuid(),
+      ...props
+    }: FieldColorProps,
+    ref: Ref<HTMLInputElement>
+  ) => {
+    const validationMessage = useFormContext(props)
+    const initialValue =
+      props.value && isValidColor(props.value)
+        ? str2simpleHsv(props.value)
+        : polarbrightness2hsv(white())
 
-  const [color, setColor] = useState<SimpleHSV>(initialValue)
-  const [inputTextValue, setInputTextValue] = useState(props.value || '')
+    const [color, setColor] = useState<SimpleHSV>(initialValue)
+    const [inputTextValue, setInputTextValue] = useState(props.value || '')
 
-  const callOnChange = () => {
-    if (!props.onChange || !color) return
-    props.onChange(createEventWithHSVValue(color))
-  }
+    const callOnChange = () => {
+      if (!props.onChange || !color) return
+      props.onChange(createEventWithHSVValue(color))
+    }
 
-  const setColorState = (newColor: SimpleHSV) => {
-    setColor(newColor)
-    newColor && setInputTextValue(simpleHSVtoFormattedColorString(newColor))
-    callOnChange()
-  }
+    const setColorState = (newColor: SimpleHSV) => {
+      setColor(newColor)
+      newColor && setInputTextValue(simpleHSVtoFormattedColorString(newColor))
+      callOnChange()
+    }
 
-  const handleColorChange = (hs: HueSaturation) =>
-    setColorState({ ...hs, v: color.v })
+    const handleColorChange = (hs: HueSaturation) =>
+      setColorState({ ...hs, v: color.v })
 
-  const handleSliderChange = (event: FormEvent<HTMLInputElement>) =>
-    setColorState({
-      ...color,
-      v: Number(event.currentTarget.value) / 100,
-    })
+    const handleSliderChange = (event: FormEvent<HTMLInputElement>) =>
+      setColorState({
+        ...color,
+        v: Number(event.currentTarget.value) / 100,
+      })
 
-  const handleInputTextChange = (event: FormEvent<HTMLInputElement>) => {
-    const value = event.currentTarget.value
-    setInputTextValue(value)
+    const handleInputTextChange = (event: FormEvent<HTMLInputElement>) => {
+      const value = event.currentTarget.value
+      setInputTextValue(value)
 
-    if (!value || !isValidColor(value)) return
+      if (!value || !isValidColor(value)) return
 
-    setColor(str2simpleHsv(value))
-    callOnChange()
-  }
+      setColor(str2simpleHsv(value))
+      callOnChange()
+    }
 
-  const content = (
-    <PopoverContent display="flex" flexDirection="column">
-      <ColorWheel
-        size={colorWheelSize}
-        hue={color.h}
-        saturation={color.s}
-        value={color.v}
-        onColorChange={handleColorChange}
-      />
-      <LuminositySlider
-        min={0}
-        max={100}
-        step={1}
-        value={color.v * 100}
-        width={colorWheelSize}
-        onChange={handleSliderChange}
-      />
-    </PopoverContent>
-  )
+    const content = (
+      <PopoverContent display="flex" flexDirection="column">
+        <ColorWheel
+          size={colorWheelSize}
+          hue={color.h}
+          saturation={color.s}
+          value={color.v}
+          onColorChange={handleColorChange}
+        />
+        <LuminositySlider
+          min={0}
+          max={100}
+          step={1}
+          value={color.v * 100}
+          width={colorWheelSize}
+          onChange={handleSliderChange}
+        />
+      </PopoverContent>
+    )
 
-  return (
-    <Field
-      {...props}
-      alignValidationMessage={alignValidationMessage || 'bottom'}
-    >
-      <FormControl alignLabel="left">
-        <Popover content={content}>
-          {(onClick, ref) => (
-            <Swatch
-              mt="auto"
+    return (
+      <Field
+        id={id}
+        validationMessage={validationMessage}
+        alignValidationMessage={alignValidationMessage || 'bottom'}
+        {...pickFieldProps(props)}
+      >
+        <FormControl alignLabel="left">
+          <Popover content={content}>
+            {(onClick, ref) => (
+              <Swatch
+                mt="auto"
+                ref={ref}
+                color={hsv2hex(color)}
+                borderRadius={hideInput ? 'medium' : 'none'}
+                borderTopLeftRadius="medium"
+                borderBottomLeftRadius="medium"
+                borderRight={!hideInput ? 'none' : undefined}
+                width="33px"
+                height="28px"
+                onClick={onClick}
+              />
+            )}
+          </Popover>
+          {!hideInput && (
+            <InputText
+              {...omitFieldProps(props)}
+              id={id}
               ref={ref}
-              color={hsv2hex(color)}
-              borderRadius={hideInput ? 'medium' : 'none'}
-              borderTopLeftRadius="medium"
-              borderBottomLeftRadius="medium"
-              borderRight={!hideInput ? 'none' : undefined}
-              width="33px"
-              height="28px"
-              onClick={onClick}
+              borderRadius="none"
+              borderTopRightRadius="medium"
+              borderBottomRightRadius="medium"
+              validationType={validationMessage && validationMessage.type}
+              onChange={handleInputTextChange}
+              value={inputTextValue}
             />
           )}
-        </Popover>
-        {!hideInput && (
-          <InputText
-            borderRadius="none"
-            borderTopRightRadius="medium"
-            borderBottomRightRadius="medium"
-            validationType={validationMessage && validationMessage.type}
-            onChange={handleInputTextChange}
-            value={inputTextValue}
-          />
-        )}
-      </FormControl>
-    </Field>
-  )
-}
+        </FormControl>
+      </Field>
+    )
+  }
+)
+
+FieldColorComponent.displayName = 'FieldColorComponent'
+
+export const FieldColor = styled(FieldColorComponent)``
