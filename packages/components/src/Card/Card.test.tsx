b11e81195f51a2fe3b7cbeb02fac6414dc973028
Luke Bowerman <luke@looker.com>
2019-10-30 20:05:38 -0700

    Cards are NOT raised by default
    Card's raised value was switched from false to true during the refactor. Put it back and set-up test to catch this change in the future by not explicitly setting raised={false} in the snapshot test.

--- packages/components/src/Card/Card.test.tsx
+++ packages/components/src/Card/Card.test.tsx
@@ -3,17 +3,17 @@
  MIT License
 
  Copyright (c) 2019 Looker Data Sciences, Inc.
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in all
  copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -31,20 +31,20 @@ import { Card } from './Card'
 import { CardMedia } from './CardMedia'
 
 test('A Card', () => {
-  const component = createWithTheme(<Card raised={false}>🥑</Card>)
+  const component = createWithTheme(<Card>🥑</Card>)
   const tree = component.toJSON()
   expect(tree).toMatchSnapshot()
 })
 
 test('A raised Card', () => {
-  const component = createWithTheme(<Card raised={true}>🥑</Card>)
+  const component = createWithTheme(<Card raised>🥑</Card>)
   const tree = component.toJSON()
   expect(tree).toMatchSnapshot()
 })
 
 test('A raised Card with a CardMedia block', () => {
   const component = createWithTheme(
-    <Card raised={true}>
+    <Card raised>
       <CardMedia image="http://placekitten.com/200/300" />
     </Card>
   )
