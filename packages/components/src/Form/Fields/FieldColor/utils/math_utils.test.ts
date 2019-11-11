7d0483e9d471100a1f0aeef1f3bd217f3cbbca80
Luke Bowerman <luke@looker.com>
2019-11-11 11:18:17 -0800

    Apply minor prettier change
    NOTE: There’s a minor version mismatch between VS Code’s prettier plug-in and our package. Hopefully VS Code will catch up shortly.

--- packages/components/src/Form/Fields/FieldColor/utils/math_utils.test.ts
+++ packages/components/src/Form/Fields/FieldColor/utils/math_utils.test.ts
@@ -3,17 +3,17 @@
  MIT License
 
  Copyright (c) 2019 Looker Data Sciences, Inc.
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in all
  copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -24,15 +24,19 @@
 
  */
 
-import { toPerc } from './math_utils'
+import { toPercent } from './math_utils'
 
 describe('math_utils', () => {
   const correctValues = [60, 24, 10, 13, 25]
-  test('toPerc', () => {
-    ;[[60, 100], [60, 255], [25, 255], [32, 255], [96, 384]].map(
-      (values, index) => {
-        expect(toPerc(values[0], values[1])).toBe(correctValues[index])
-      }
-    )
+  test('toPercent', () => {
+    ;[
+      [60, 100],
+      [60, 255],
+      [25, 255],
+      [32, 255],
+      [96, 384],
+    ].map((values, index) => {
+      expect(toPercent(values[0], values[1])).toBe(correctValues[index])
+    })
   })
 })
