6adf0960d70fdeb516a825161cbc7db09b9d2f81
Lara Harrow <lara.harrow@looker.com>
2019-11-13 14:21:41 -0800

    AvatarUser added to components

---
+++ packages/components/src/Avatar/AvatarUser.tsx
@@ -0,0 +1,117 @@
+/*
+
+ MIT License
+
+ Copyright (c) 2019 Looker Data Sciences, Inc.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ */
+
+import React, { FC } from 'react'
+// import { IconNames } from '@looker/icons'
+
+export interface AvatarProps {
+  /**
+   *  @default `2rem`
+   **/
+  size?: number
+  /**
+   *  @default `palette.purple500`
+   **/
+  color?: string
+}
+
+interface AvatarUserProps extends AvatarProps {
+  user: {
+    first_name: string | null
+    last_name: string | null
+    avatar_url: string | null
+  }
+}
+
+interface FakeData {
+  [key: string]: string | number
+}
+
+const userWithImage: FakeData = {
+  avatarUrl:
+    'https://gravatar.lookercdn.com/avatar/e8ebbdf1a644117215036eac62995731?s=156&d=blank',
+  firstName: 'Luke',
+  id: 61,
+  lastName: 'Bowerman',
+}
+
+const userWithWrongURL: FakeData = {
+  avatarUrl:
+    'https://gravatar.lookercdn.com/avatar/e8ebbdf1a64411721503995731?s=156&d=blank',
+  firstName: 'John',
+  id: 62,
+  lastName: 'Smith',
+}
+
+export const AvatarUser: FC<AvatarUserProps> = ({
+  size = '2rem',
+  color = 'palette.purple500',
+  user,
+}) => {
+  const initials =
+    user.first_name &&
+    user.last_name &&
+    `${user.first_name.substr(0, 1)}${user.last_name.substr(0, 1)}`
+}
+
+// const SpinnerFactory: FC<SpinnerProps> = props => {
+//   const {
+//     color = 'palette.charcoal900',
+//     markers = 13,
+//     markerRadius,
+//     speed = 1000,
+//   } = props
+//   return (
+//     <Style {...omit(props, 'color', 'markers', 'markersRadius', 'speed')}>
+//       {range(markers).map(i => (
+//         <SpinnerMarker
+//           backgroundColor={color}
+//           key={i}
+//           speed={speed}
+//           markers={markers}
+//           markerIndex={i}
+//           markerRadius={markerRadius}
+//         />
+//       ))}
+//     </Style>
+//   )
+// }
+
+// const Style = styled.div<SpinnerProps>`
+//   ${reset}
+//   ${space}
+//   ${position}
+
+//   height: ${props => props.size}px;
+//   position: relative;
+//   width: ${props => props.size}px;
+// `
+
+// Style.defaultProps = {
+//   size: 30,
+// }
+
+// export const Spinner = styled(SpinnerFactory)``
