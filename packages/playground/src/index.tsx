10f0e06d67608b316216e65762f75ff67532c3e3
Meredith Dodge <meredith.dodge@looker.com>
2020-01-09 12:54:08 -0800

    remove tooltip demo

--- packages/playground/src/index.tsx
+++ packages/playground/src/index.tsx
@@ -24,13 +24,13 @@ import { GlobalStyle } from '@looker/components'
 import { theme } from '@looker/design-tokens'
 import { ThemeProvider } from 'styled-components'
 
-import { TooltipDemo } from './Tooltip/TooltipDemo'
+import { ConfirmDemo } from './Confirm/ConfirmDemo'
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <>
         <GlobalStyle />
-        <TooltipDemo />
+        <ConfirmDemo />
       </>
     </ThemeProvider>
   )
