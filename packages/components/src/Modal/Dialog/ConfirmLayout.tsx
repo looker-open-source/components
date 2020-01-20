37a6d85938d3e4bd593dcd9f57cc8dc2956a8bb4
Daniel Christopher <dbchristopher@gmail.com>
2020-01-20 15:02:59 -0800

    [LENS-502] Confirm Close Dialog Example (#400)

---
+++ packages/components/src/Modal/Dialog/ConfirmLayout.tsx
@@ -0,0 +1,54 @@
+import React, { FC, ReactElement } from 'react'
+
+import { ButtonProps } from '../../Button'
+import { IconProps } from '../../Icon'
+import { Paragraph } from '../../Text'
+import { ModalContent, ModalFooter, ModalHeader } from '../Layout'
+
+interface ConfirmLayoutProps {
+  /**
+   * Header content
+   */
+  title: string
+  /**
+   * Optional icon to display next to the title
+   */
+  titleIcon?: ReactElement<IconProps>
+  /**
+   * Primary modal content
+   */
+  message: ReactElement | string
+  /**
+   * Click to confirm
+   */
+  primaryButton: ReactElement<ButtonProps>
+  /**
+   * Click to cancel
+   */
+  secondaryButton: ReactElement<ButtonProps>
+}
+
+export const ConfirmLayout: FC<ConfirmLayoutProps> = ({
+  secondaryButton,
+  primaryButton,
+  message,
+  title,
+  titleIcon = undefined,
+}) => {
+  return (
+    <>
+      <ModalHeader headerIcon={titleIcon}>{title}</ModalHeader>
+      <ModalContent innerProps={{ py: 'none' }}>
+        {typeof message === 'string' ? (
+          <Paragraph>{message}</Paragraph>
+        ) : (
+          message
+        )}
+      </ModalContent>
+      <ModalFooter>
+        {primaryButton}
+        {secondaryButton}
+      </ModalFooter>
+    </>
+  )
+}
