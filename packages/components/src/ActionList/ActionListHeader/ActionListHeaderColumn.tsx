bedda138cb2a38b9458307e58fd3364baa5aac1d
Elliot Park <elliot.jun.park@gmail.com>
2020-02-10 11:39:52 -0800

    Removing dummy prop in ActionListHeader, adding padding to ActionListHeaderColumn, adding new files for ActionListItem work

--- packages/components/src/ActionList/ActionListHeader/ActionListHeaderColumn.tsx
+++ packages/components/src/ActionList/ActionListHeader/ActionListHeaderColumn.tsx
@@ -1,3 +1,10 @@
+import { space, SpaceProps } from '@looker/design-tokens'
 import styled from 'styled-components'
 
-export const ActionListHeaderColumn = styled.div``
+export const ActionListHeaderColumn = styled.div<SpaceProps>`
+  ${space}
+`
+
+ActionListHeaderColumn.defaultProps = {
+  px: 'small',
+}
