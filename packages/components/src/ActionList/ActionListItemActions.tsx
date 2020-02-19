fdd8909c640748ea87fc8f22e3492dbce285afd9
Elliot Park <elliot.jun.park@gmail.com>
2020-02-18 16:21:56 -0800

    Prep work for hoverDisclosureRef implementation

--- packages/components/src/ActionList/ActionListItemActions.tsx
+++ packages/components/src/ActionList/ActionListItemActions.tsx
@@ -1,13 +1,31 @@
 import styled from 'styled-components'
+import React from 'react'
 import { Icon } from '../Icon'
+import { Menu, MenuDisclosure, MenuItem, MenuList } from '../Menu'
 
-// TODO: Use IconButton
-export const ActionListItemActions = styled(Icon).attrs(() => {
-  return {
-    name: 'DotsVert',
-    size: 36,
-  }
-})`
-  height: 100%;
+export interface ActionListItemActionsProps {
+  className?: string
+}
+
+const ActionListItemActionsInternal = (props: ActionListItemActionsProps) => {
+  return (
+    <Menu>
+      <MenuDisclosure>
+        <Icon className={props.className} name="DotsVert" size={36} />
+      </MenuDisclosure>
+      {/*
+        Leaving placeholder MenuItem elements in for now
+        Final iteration will instead render props.children
+      */}
+      <MenuList>
+        <MenuItem>Gouda</MenuItem>
+        <MenuItem>American</MenuItem>
+        <MenuItem>Swiss</MenuItem>
+      </MenuList>
+    </Menu>
+  )
+}
+
+export const ActionListItemActions = styled(ActionListItemActionsInternal)`
   cursor: pointer;
 `
