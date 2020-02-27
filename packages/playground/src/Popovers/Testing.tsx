c72fc52ca3560d37b6cfe0fe45d1475a41e694ce
Meredith Dodge <meredith.dodge@looker.com>
2020-02-27 12:27:18 -0800

    Add Popovers Testing page to playground

--- packages/playground/src/Popovers/Testing.tsx
+++ packages/playground/src/Popovers/Testing.tsx
@@ -1,14 +1,19 @@
 /*
+
  MIT License
+
  Copyright (c) 2019 Looker Data Sciences, Inc.
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
+
  The above copyright notice and this permission notice shall be included in all
  copies or substantial portions of the Software.
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -16,29 +21,45 @@
  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  SOFTWARE.
+
  */
 
 import React from 'react'
-import ReactDOM from 'react-dom'
-import { GlobalStyle } from '@looker/components'
-import { theme } from '@looker/design-tokens'
-import { ThemeProvider } from 'styled-components'
+import {
+  Box,
+  Button,
+  DialogManager,
+  Menu,
+  MenuDisclosure,
+  MenuList,
+  MenuItem,
+  ModalContent,
+  Paragraph,
+} from '@looker/components'
 
-import { TestPopovers } from './Popovers/Testing'
-
-const App: React.FC = () => {
+export function TestPopovers() {
+  function openAlert() {
+    alert(`It's working!`)
+  }
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <TestPopovers />
-    </ThemeProvider>
+    <Box m="large">
+      <Menu>
+        <MenuDisclosure tooltip="Select your favorite kind">
+          <Button mb="large">Open Menu</Button>
+        </MenuDisclosure>
+        <MenuList>
+          <DialogManager
+            content={
+              <ModalContent>
+                <Paragraph>Some content inside the Dialog</Paragraph>
+                <Button onClick={openAlert}>Open Alert</Button>
+              </ModalContent>
+            }
+          >
+            {onClick => <MenuItem onClick={onClick}>Open Modal</MenuItem>}
+          </DialogManager>
+        </MenuList>
+      </Menu>
+    </Box>
   )
 }
-
-/**
- * This is the binding site for the playground. If you want to edit the
- * primary application, do your work in App.tsx instead.
- */
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<App />, document.getElementById('container'))
-})
