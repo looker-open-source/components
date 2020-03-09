d1502cdbef88e7e36d9ff7ea6183719caff7c3b4
Meredith Dodge <meredith.dodge@looker.com>
2020-03-09 11:12:12 -0700

    Weird IconButton import bug

--- packages/playground/src/index.tsx
+++ packages/playground/src/index.tsx
@@ -24,13 +24,13 @@ import { GlobalStyle } from '@looker/components'
 import { theme } from '@looker/design-tokens'
 import { ThemeProvider } from 'styled-components'
 
-import { InputDateDemo } from './InputDate/InputDateDemo'
+import { ComboboxDemo } from './Select/ComboboxDemo'
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <InputDateDemo />
+      <ComboboxDemo />
     </ThemeProvider>
   )
 }
