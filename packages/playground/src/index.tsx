66e66616b5a1c02fb79a2b57d34d2d58df8c7794
Daniel Christopher <dbchristopher@gmail.com>
2020-03-31 16:05:53 -0700

    pass prop directly to styled element

--- packages/playground/src/index.tsx
+++ packages/playground/src/index.tsx
@@ -24,14 +24,14 @@ import { GlobalStyle } from '@looker/components'
 import { theme } from '@looker/design-tokens'
 import { ThemeProvider } from 'styled-components'
 
-import { ActionListDemo } from './ActionList/ActionListDemo'
+import { TextAreaDemo } from './Form/TextAreaDemo'
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <>
         <GlobalStyle />
-        <ActionListDemo />
+        <TextAreaDemo />
       </>
     </ThemeProvider>
   )
