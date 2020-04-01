a01b22acf18d6a5eee02a7b872928b2608f27467
Luke Bowerman <34253496+lukelooker@users.noreply.github.com>
2020-04-01 15:46:05 -0700

    Added CodeBlock component (#717)
    * Added CodeBlock component

--- packages/components/src/Text/CodeBlock.tsx
+++ packages/components/src/Text/CodeBlock.tsx
@@ -1,19 +1,14 @@
 /*
-
  MIT License
-
  Copyright (c) 2020 Looker Data Sciences, Inc.
-
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
-
  The above copyright notice and this permission notice shall be included in all
  copies or substantial portions of the Software.
-
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,12 +16,31 @@
  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  SOFTWARE.
-
  */
 
-export * from './Code'
-export * from './CodeBlock'
-export * from './Heading'
-export * from './Paragraph'
-export * from './ReplaceText'
-export * from './Text'
+import { BorderProps } from '@looker/design-tokens'
+import React, { FC } from 'react'
+import styled from 'styled-components'
+import { TextBase, TextBaseProps } from './TextBase'
+
+export interface CodeBlockProps extends TextBaseProps, BorderProps {}
+
+const CodeBlockLayout: FC<CodeBlockProps> = ({ children, ...props }) => (
+  <TextBase as="pre" {...props}>
+    <code>{children}</code>
+  </TextBase>
+)
+
+export const CodeBlock = styled(CodeBlockLayout)`
+  border: 1px solid;
+  border-color: ${(props) => props.theme.colors.palette.charcoal200};
+  overflow-y: scroll;
+`
+
+CodeBlock.defaultProps = {
+  border: '1px solid',
+  borderColor: 'palette.charcoal200',
+  fontFamily: 'code',
+  fontSize: 'small',
+  p: 'medium',
+}
