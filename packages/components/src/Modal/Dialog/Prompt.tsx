61e0fd794f3306ae75cbd68d0ff8b6ccf438c798
Elliot Park <elliot.jun.park@gmail.com>
2020-04-08 14:51:26 -0700

    Added usePrompt hook

--- packages/components/src/Modal/Dialog/Prompt.tsx
+++ packages/components/src/Modal/Dialog/Prompt.tsx
@@ -1,9 +1,25 @@
 import React, { FC, ReactNode } from 'react'
+import { useToggle } from '../../utils'
 import { PromptDialog, PromptDialogProps } from './PromptDialog'
-import { DialogManager } from '.'
+import { Dialog, DialogManager } from '.'
 
 export interface PromptProps extends PromptDialogProps {
   children: (onClick: () => void) => ReactNode
+}
+
+export function usePrompt(props: PromptDialogProps): [ReactNode, () => void] {
+  // Needs to return two things
+  // 1. The actual modal
+  // 2. A way to open the modal (closing the modal can be taken care of within PromptDialog)
+  const { value, setOn, setOff } = useToggle()
+
+  const rendered = (
+    <Dialog width="30rem" isOpen={value} onClose={setOff}>
+      <PromptDialog {...props} />
+    </Dialog>
+  )
+
+  return [rendered, setOn]
 }
 
 export const Prompt: FC<PromptProps> = ({ children, ...props }) => (
