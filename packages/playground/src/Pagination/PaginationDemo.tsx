38f3b4e36a90e14a4877983634279c388a4886c2
Elliot Park <elliot.jun.park@gmail.com>
2020-04-09 15:41:50 -0700

    Added Pagination component

--- packages/playground/src/Pagination/PaginationDemo.tsx
+++ packages/playground/src/Pagination/PaginationDemo.tsx
@@ -1,14 +1,19 @@
 /*
+
  MIT License
+
  Copyright (c) 2020 Looker Data Sciences, Inc.
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
+
  The above copyright notice and this permission notice shall be included in all
  copies or substantial portions of the Software.
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -16,28 +21,24 @@
  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  SOFTWARE.
+
  */
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { GlobalStyle } from '@looker/components'
-import { theme } from '@looker/design-tokens'
-import { ThemeProvider } from 'styled-components'
-import { PaginationDemo } from './Pagination/PaginationDemo'
+import React, { FC, useState } from 'react'
+import { Flex, Pagination } from '@looker/components'
 
-const App: React.FC = () => {
+export const PaginationDemo: FC = () => {
+  const [currentPage, setCurrentPage] = useState(1)
+
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <PaginationDemo />
-    </ThemeProvider>
+    <Flex
+      flexDirection="column"
+      justifyContent="space-around"
+      alignItems="center"
+      height="300px"
+    >
+      <h1>{`You're currently viewing page ${currentPage}`}</h1>
+      <Pagination current={currentPage} pages={10} onChange={setCurrentPage} />
+    </Flex>
   )
 }
-
-/*
-  This is the binding site for the playground. If you want to edit the
-  primary application, do your work in App.tsx instead.
- */
-document.addEventListener('DOMContentLoaded', () => {
-  ReactDOM.render(<App />, document.getElementById('container'))
-})
