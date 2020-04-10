4f084c695230ee05a0e22a9ba9e20064de346bca
Elliot Park <elliot.jun.park@gmail.com>
2020-04-10 09:48:24 -0700

    Initial commit of PageSize-related files - Included component file, test file, and index

---
+++ packages/components/src/PageSize/PageSize.tsx
@@ -0,0 +1,80 @@
+/*
+
+ MIT License
+
+ Copyright (c) 2020 Looker Data Sciences, Inc.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ */
+
+import React, { FC } from 'react'
+import styled from 'styled-components'
+import { Select } from '../Form'
+
+export interface PageSizeProps {
+  total: number
+  value: number
+  options?: number[]
+  className?: string
+  onChange: (value: number) => Promise<void>
+}
+
+const defaultPageSizes = [10, 25, 50, 100]
+
+const stringToSelectOption = (option: string) => ({
+  label: String(option),
+  value: String(option),
+})
+
+const arrayToSelectOptions = (options: Array<string | number>) =>
+  options.map((option) => stringToSelectOption(String(option)))
+
+export const PageSizeLayout: FC<PageSizeProps> = ({
+  value,
+  total,
+  className,
+  onChange,
+  options,
+}) => {
+  options = options || defaultPageSizes
+
+  const handleOnChange = async (newValue: string) => {
+    await onChange(Number(newValue))
+  }
+
+  return total > value ? (
+    <div className={className}>
+      Display
+      <Select
+        width="5rem"
+        mx="medium"
+        options={arrayToSelectOptions(options)}
+        value={String(value)}
+        onChange={handleOnChange}
+      />
+      of {total}
+    </div>
+  ) : null
+}
+
+export const PageSize = styled(PageSizeLayout)`
+  display: flex;
+  align-items: center;
+`
