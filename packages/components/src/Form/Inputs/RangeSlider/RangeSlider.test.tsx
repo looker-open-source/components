2030fc624d8c31fec91f5988d2d3c92920e429a8
Daniel Christopher <dbchristopher@gmail.com>
2020-04-29 12:22:42 -0700

    test boilerplate

--- packages/components/src/Form/Inputs/RangeSlider/RangeSlider.test.tsx
+++ packages/components/src/Form/Inputs/RangeSlider/RangeSlider.test.tsx
@@ -23,3 +23,36 @@
  SOFTWARE.
 
  */
+
+// import React from 'react'
+// import { fireEvent } from '@testing-library/react'
+// import { renderWithTheme } from '@looker/components-test-utils'
+// import { RangeSlider } from './RangeSlider'
+
+// const globalConsole = global.console
+
+// beforeEach(() => {
+//   global.console = ({
+//     error: jest.fn(),
+//     warn: jest.fn(),
+//   } as unknown) as Console
+// })
+
+// afterEach(() => {
+//   jest.resetAllMocks()
+//   global.console = globalConsole
+// })
+
+// test('it selects the entire range by default', () => {})
+
+// test('warns the developer if value prop falls outside of possible min/max range', () => {})
+
+// test('fires onChange callback when slider is interacted with', () => {})
+
+// test('increments point by STEP value during keyboard navigation', () => {})
+
+// test('prevents range from going outside of min/max values', () => {})
+
+// test('disabled component does not respond to keyboard or mouse inputs', () => {})
+
+// test('readOnly component does not respond to keyboard or mouse inputs', () => {})
