51d15395bde72d93ed949ae2523b34e5f38c6205
Elliot Park <elliot.jun.park@gmail.com>
2020-06-04 11:35:14 -0700

    Tooltip now clones child with hover class if tooltip is Open

--- packages/playground/src/index.tsx
+++ packages/playground/src/index.tsx
@@ -25,15 +25,21 @@
  */
 import React from 'react'
 import { render } from 'react-dom'
-import { ComponentsProvider, InputColor, Swatch } from '@looker/components'
+import { ComponentsProvider, Tooltip, Button } from '@looker/components'
 
 const App = () => {
   return (
     <ComponentsProvider>
-      <InputColor />
-      <InputColor value="green" />
-      <InputColor defaultValue="purple" />
-      <Swatch />
+      <div>
+        <Tooltip content="Hello Darkness My Old Friend" placement="right">
+          <Button>Hello World</Button>
+        </Tooltip>
+      </div>
+      <div>
+        <Tooltip content="Hello Darkness My Old Friend" placement="right">
+          {(props) => <Button {...props}>Hello World</Button>}
+        </Tooltip>
+      </div>
     </ComponentsProvider>
   )
 }
