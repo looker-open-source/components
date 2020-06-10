5387ffb145cf5d01310d3a7f73189cc90d42da43
Lara Harrow <lara.harrow@looker.com>
2020-06-10 10:53:07 -0700

    artwork ready for review

--- packages/components/src/Icon/Icon.tsx
+++ packages/components/src/Icon/Icon.tsx
@@ -50,6 +50,9 @@ export type IconSize =
 export interface IconProps
   extends Omit<CompatibleHTMLProps<HTMLDivElement>, 'onClick'>,
     SimpleLayoutProps {
+  /**
+   * Display an icon/logo that is not available on our components list. Use artwork prop with an svg instead of Icon name.
+   */
   artwork?: ReactNode
   color?: string
   name?: IconNames
@@ -64,9 +67,7 @@ const IconFactory = forwardRef(
   ) => {
     if ((artwork && name) || (!artwork && !name)) {
       // eslint-disable-next-line no-console
-      console.warn(
-        "Please pass ether name or artwork as Icon prop. If both are passed the default will be artwork. If non is passed Icon won't be displayed"
-      )
+      console.warn('You may only specify name OR artwork, not both.')
     }
     const Glyph = name ? Glyphs[name] : 'div'
     const value = artwork || (
@@ -92,4 +93,9 @@ export const Icon = styled(IconFactory).attrs(({ size, height, width }) => ({
   display: inline-flex;
   height: ${({ height }) => height};
   width: ${({ width }) => width};
+
+  svg {
+    height: 100%;
+    width: 100%;
+  }
 `
