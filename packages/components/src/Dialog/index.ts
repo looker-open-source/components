371c37073984e762455fb9a6bb98de84c327828d
Luke Bowerman <34253496+lukelooker@users.noreply.github.com>
2020-06-24 19:58:10 -0700

    Modal & Portal fixes (#1131)

--- packages/components/src/Dialog/index.ts
+++ packages/components/src/Dialog/index.ts
@@ -26,7 +26,7 @@
 
 export * from './Confirm'
 export * from './Dialog'
-export * from './Layout'
 export * from './DialogContext'
 export * from './DialogManager'
+export * from './Layout'
 export * from './Prompt'
