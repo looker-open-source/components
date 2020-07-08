8abb819fb34daaac585bf368a2bb6b155c63aef5
Daniel Christopher <dbchristopher@gmail.com>
2020-07-08 17:01:44 +0000

    messagebar component feedback

--- packages/components/src/MessageBar/MessageBar.tsx
+++ packages/components/src/MessageBar/MessageBar.tsx
@@ -45,6 +45,7 @@ export interface MessageBarProps
     SimpleLayoutProps,
     TypographyProps {
   /**
+   * Determins the icon choice and background color
    * @default: 'inform'
    */
   intent?: MessageBarIntent
@@ -53,9 +54,16 @@ export interface MessageBarProps
    * @default: true
    */
   canDismiss?: boolean
+  /**
+   * Called after internal `visible` state is updated
+   */
   onDismiss?: () => void
-  className?: string
+  /**
+   * Determines whether the MessageBar is rendered or not. Used in conjunction with the onDismiss prop
+   * @default: true
+   */
   visible?: boolean
+  className?: string
 }
 
 const MessageBarLayout = forwardRef(
@@ -88,34 +96,32 @@ const MessageBarLayout = forwardRef(
       }
     }, [visibleProp])
 
-    if (visible) {
-      return (
-        <div
-          aria-live="polite"
-          ref={ref}
-          role="status"
-          {...omitStyledProps(props)}
-        >
-          <Status intent={intent} />
-          <MessageBarContent canDismiss={canDismiss}>
-            {children}
-          </MessageBarContent>
-          {canDismiss && (
-            <IconButton
-              id={id ? `${id}-iconButton` : undefined}
-              ml="auto"
-              onClick={handleDismiss}
-              icon="Close"
-              size="small"
-              label={`Dismiss ${getIntentLabel(intent)}`}
-              aria-hidden
-            />
-          )}
-        </div>
-      )
-    } else {
-      return null
-    }
+    const messageBarMarkup = (
+      <div
+        aria-live="polite"
+        ref={ref}
+        role="status"
+        {...omitStyledProps(props)}
+      >
+        <Status intent={intent} />
+        <MessageBarContent canDismiss={canDismiss}>
+          {children}
+        </MessageBarContent>
+        {canDismiss && (
+          <IconButton
+            id={id ? `${id}-iconButton` : undefined}
+            ml="auto"
+            onClick={handleDismiss}
+            icon="Close"
+            size="small"
+            label={`Dismiss ${getIntentLabel(intent)}`}
+            aria-hidden
+          />
+        )}
+      </div>
+    )
+
+    return visible ? messageBarMarkup : null
   }
 )
 
