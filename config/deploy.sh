d876b5f46e0078106a783f7f27f10c06c4091fed
Luke Bowerman <34253496+lukelooker@users.noreply.github.com>
2020-07-09 22:22:07 -0700

    Deploy to a subfolder (#1191)
    * Deploy to a subfolder
    * Remove references to subdomain, disable to prevent CI failures

--- config/deploy.sh
+++ config/deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-mkdir -p public
+# clean-up previous build
+rm -rf public
+mkdir public
 
 yarn workspace storybook build
+
+# Build Gatsby, then move contents to public folder for publishing
 yarn workspace www build
 mv www/public/* public
 
-echo "components.looker.com" > public/CNAME
+#echo \"components.looker.com\" > public/CNAME
 yarn gh-pages -d public -r https://$GITHUB_TOKEN@github.com/looker-open-source/components.git
