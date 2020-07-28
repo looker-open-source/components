1acd586c0d7b640eb1919fd5025432a55acbd7e4
Elliot Park <elliotpark@google.com>
2020-07-28 22:56:04 +0000

    Replaced existing dummy function with lodash/noop in ActionListContext

--- packages/components/src/ActionList/ActionListContext.tsx
+++ packages/components/src/ActionList/ActionListContext.tsx
@@ -25,6 +25,7 @@
  */
 
 import { createContext } from 'react'
+import noop from 'lodash/noop'
 import { MixedBoolean } from '../Form'
 import { ActionListColumns } from './ActionList'
 
@@ -42,11 +43,7 @@ export interface ActionListContextProps {
 }
 
 export const ActionListContext = createContext<ActionListContextProps>({
-  addItemToAllItems: (id: string) => {
-    // Using noop; addItemToAllItems is passed from ActionList component into context
-    // eslint-disable-next-line no-console
-    console.log(id)
-  },
+  addItemToAllItems: (id: string) => noop(id),
   allItems: [],
   canSelect: false,
   itemsSelected: [],
