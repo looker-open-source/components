52842fad1743e8e1935260f004204b72a551f701
Elliot Park <elliotpark@google.com>
2020-07-30 16:45:37 +0000

    Modified onSelect in bulk actions example - Clicking on individual  checkbox while non-displayed items are selected will lead to only displayed items being included in selections

--- storybook/src/ActionList/ActionListBulkActions.stories.tsx
+++ storybook/src/ActionList/ActionListBulkActions.stories.tsx
@@ -34,25 +34,26 @@ export default {
 }
 
 export const BulkActions = () => {
-  const [selections, setSelections] = useState([] as string[])
+  const [selections, setSelections] = useState<string[]>([])
+
+  const allPageItems = data.map(({ pdtName }) => pdtName)
+
   const onSelect = (selection: string) => {
+    // Note: In the event that selections includes the item being selected, we call filter on allPageItems.
+    // This is to avoid the situation where you have non-displayed items selected but only some displayed items.
+    // Doing the above will mean you have selected items that cannot be unselected (i.e. there's no way to interact with non-displayed items).
     setSelections(
       selections.includes(selection)
-        ? selections.filter((item) => item !== selection)
+        ? allPageItems.filter((item) => item !== selection)
         : [...selections, selection]
     )
   }
 
-  const allSelectableItems = data
-    .map(({ disabled, pdtName }) => !disabled && pdtName)
-    .filter((element) => element) as string[]
-
-  const onSelectAll = () =>
-    setSelections(selections.length ? [] : allSelectableItems)
+  const onSelectAll = () => setSelections(selections.length ? [] : allPageItems)
 
   const onTotalSelectAll = () =>
     setSelections([
-      ...allSelectableItems,
+      ...allPageItems,
       'some_other_pdt_1',
       'some_other_pdt_2',
       'some_other_pdt_3',
