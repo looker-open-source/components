75dd0ed7962aa29a0951fe6952d0cceef4487b8e
Lara Harrow <laraharrow@google.com>
2020-09-24 20:29:21 +0000

    first run

--- packages/components/src/Form/Inputs/InputFilters/InputFilters.tsx
+++ packages/components/src/Form/Inputs/InputFilters/InputFilters.tsx
@@ -25,13 +25,14 @@
  */
 
 import styled from 'styled-components'
-import React, { FC, useState, useRef } from 'react'
+import React, { FC, useState, useRef, ReactNode } from 'react'
 import { partition } from 'lodash'
 import { Select } from '../Select'
 import { InputText } from '../InputText'
 import { Icon } from '../../../Icon'
-import { IconButton } from '../../../Button'
+import { IconButton, Button } from '../../../Button'
 import { Chip } from '../../../Chip'
+import { Popover, PopoverContent } from '../../../Popover'
 import { InputFilterChip } from './InputFilterChip'
 
 export interface FieldFilter {
@@ -100,6 +101,11 @@ const InputFiltersLayout: FC<InputFiltersProps> = ({
     setChipValues([...chipValues].filter((value) => value !== field))
   }
 
+  const handleSecondFilterList = (event: MouseEvent) => {
+    console.log('event: ', event.currentTarget)
+    // setChipValues([...chipValues, newValue])
+  }
+
   return (
     <div className={className} onClick={focusInput}>
       {!hideFilterIcon && (
@@ -114,7 +120,17 @@ const InputFiltersLayout: FC<InputFiltersProps> = ({
             onDelete={handleDelete}
           />
         ))}
-      {draftFilter && <Chip>{draftFilter.label || draftFilter.field}</Chip>}
+      {draftFilter && (
+        <Popover
+          content={
+            <PopoverContent p="large" width="360px">
+              <Chip onClick={handleSecondFilterList}>suggestion 1</Chip>
+            </PopoverContent>
+          }
+        >
+          <Chip>{draftFilter.label || draftFilter.field}</Chip>
+        </Popover>
+      )}
       {!draftFilter && (
         <Select
           autoResize
