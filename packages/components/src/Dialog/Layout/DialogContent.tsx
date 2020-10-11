a27ae7a3df0153ae5b0088ba13043160b1cadbaf
Luke Bowerman <lukebowerman@google.com>
2020-10-11 21:00:10 +0000

    Styled Components 5 prep Test fix-ups
    Refactored DialogContent to make remove class component, removed
    problematic test and replaced with Storybook image-snapshot
    
    Remove `FieldText.stories` storybook - it wasn't useful
    
    Replaced instances of `toHaveStyle` with `toHaveStyleRule` (former
    doesn't work well with SC5)

--- packages/components/src/Dialog/Layout/DialogContent.tsx
+++ packages/components/src/Dialog/Layout/DialogContent.tsx
@@ -40,11 +40,11 @@ export interface DialogContentProps
   extends LayoutProps,
     CompatibleHTMLProps<HTMLDivElement> {}
 
-interface DialogContentLayoutProps extends DialogContentProps {
+interface DialogContentLayout extends DialogContentProps {
   renderedHeight: string
 }
 
-const DialogContentLayout: FC<DialogContentLayoutProps> = ({
+const DialogContentLayout: FC<DialogContentLayout> = ({
   children,
   className,
   renderedHeight,
@@ -68,7 +68,7 @@ const DialogContentLayout: FC<DialogContentLayoutProps> = ({
 
   return (
     <div
-      className={`${className} ${overflow ? 'overflow' : ''}`}
+      className={`${className} ${overflow && 'overflow'}`}
       ref={internalRef}
       {...omit(props, ['renderedHeight'])}
     >
