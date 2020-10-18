167d17680a2227bb607a66848280c952cc94a73a
Luke Bowerman <34253496+lukelooker@users.noreply.github.com>
2020-10-18 02:58:31 -0700

    Update tooling - jest, types, lints (#1584)
    * Update tooling - jest, types, lints
    * Reduce D3 usage, correct typing

--- packages/design-tokens/src/tokens/breakpoints.ts
+++ packages/design-tokens/src/tokens/breakpoints.ts
@@ -24,4 +24,12 @@
 
  */
 
+/**
+ * 320px — 480px: Mobile devices. (20rem - 30rem)
+ * 481px — 768px: iPads, Tablets. (30rem - 48rem)
+ * 769px — 1024px: Small screens, laptops. (48rem - 64rem)
+ * 1025px — 1200px: Desktops, large screens. (64rem - 75rem)
+ * 1201px and more — Extra large screens, TV. (90rem +)
+ **/
+
 export const breakpoints = ['30rem', '48rem', '64rem', '75rem', '90rem']
