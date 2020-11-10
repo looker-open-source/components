607a580c951ee551928eb8376cbda71384deecd7
Luke Bowerman <34253496+lukelooker@users.noreply.github.com>
2020-11-09 19:42:23 -0800

    Introduce ExtendComponentsProvider (#1636)
    * Introduce ExtendComponentsProvider

--- packages/components-providers/src/ExtendComponentsProvider.tsx
+++ packages/components-providers/src/ExtendComponentsProvider.tsx
@@ -23,6 +23,32 @@
  SOFTWARE.
 
  */
-export * from './ComponentsProvider'
-export * from './ExtendComponentsProvider'
-export * from './ScrollLock'
+
+import { generateTheme, ThemeCustomizations } from '@looker/design-tokens'
+import { ThemeContext, ThemeProvider } from 'styled-components'
+import React, { FC, useMemo, useContext } from 'react'
+
+export interface ExtendComponentsTheme {
+  themeCustomizations?: ThemeCustomizations
+}
+
+/**
+ * This component is designed for making adjustments to the Theme without
+ * initializing an entire ComponentsProvider. ExtendComponentsThemeProvider
+ * will merge the themeCustomizations with the theme already established.
+ *
+ * This component is intended for use cases where a portion of page carries
+ * a different theme than the rest.
+ */
+export const ExtendComponentsThemeProvider: FC<ExtendComponentsTheme> = ({
+  children,
+  themeCustomizations,
+}) => {
+  const parentTheme = useContext(ThemeContext)
+
+  const theme = useMemo(() => {
+    return generateTheme(parentTheme, themeCustomizations)
+  }, [parentTheme, themeCustomizations])
+
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>
+}
