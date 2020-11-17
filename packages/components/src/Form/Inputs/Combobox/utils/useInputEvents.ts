787c948af36123e3d657dc2a45334b5c75d3188b
Lara Harrow <laraharrow@google.com>
2020-11-17 20:02:50 +0000

    update snapshots

--- packages/components/src/Form/Inputs/Combobox/utils/useInputEvents.ts
+++ packages/components/src/Form/Inputs/Combobox/utils/useInputEvents.ts
@@ -68,7 +68,7 @@ export function useInputEvents<
     disabled,
     // highlights all the text in the box on click when true
     selectOnClick = false,
-    inputReadOnly = false,
+    readOnly = false,
     // wrapped events
     onClick,
     onMouseDown,
