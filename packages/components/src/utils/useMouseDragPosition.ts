d8721c7b3fb1c2ca039fedfb752080281dfd2869
Daniel Christopher <dbchristopher@gmail.com>
2020-12-01 00:25:13 +0000

    DRY iseMouseDragPosition

--- packages/components/src/utils/useMouseDragPosition.ts
+++ packages/components/src/utils/useMouseDragPosition.ts
@@ -26,6 +26,7 @@
 
 import { useState, useEffect } from 'react'
 import throttle from 'lodash/throttle'
+import get from 'lodash/get'
 
 interface MouseState {
   mousePos: { x: number; y: number }
@@ -42,55 +43,48 @@ export function useMouseDragPosition(
   const [isMouseDown, setIsMouseDown] = useState(false)
   const [mousePos, setMousePos] = useState({ x: 0, y: 0 })
 
-  const handleMouseDown = (e: globalThis.MouseEvent) => {
-    setMousePos({ x: e.pageX, y: e.pageY })
-    setIsMouseDown(true)
-  }
-
-  const handleMouseUp = () => {
-    setIsMouseDown(false)
-  }
-
-  const handleMouseMove = (e: globalThis.MouseEvent) => {
-    if (isMouseDown) {
-      // e.clientX and e.clientY fallbacks are included for testing purposes. jsDOM doesn't support pageX/pageY attributes
-      setMousePos({ x: e.pageX || e.clientX, y: e.pageY || e.clientY })
-    }
-  }
-
-  const handleTouchMove = throttle((e: globalThis.TouchEvent) => {
-    const { pageX, clientX, pageY, clientY } = e.touches[0] // e.clientX and e.clientY fallbacks are included for testing purposes. jsDOM doesn't support pageX/pageY attributes
-    setMousePos({ x: pageX || clientX, y: pageY || clientY })
-  }, 50)
-
-  const handleTouchStart = (e: globalThis.TouchEvent) => {
+  const updateMousePos = (e: globalThis.TouchEvent | globalThis.MouseEvent) => {
+    // use e.touches[0] for touch events, or simply e for mouse events
+    const event = get(e, 'touches[0]', e)
     // e.clientX and e.clientY fallbacks are included for testing purposes. jsDOM doesn't support pageX/pageY attributes
-    const { pageX, clientX, pageY, clientY } = e.touches[0]
-    setIsMouseDown(true)
+    const { pageX, clientX, pageY, clientY } = event
     setMousePos({ x: pageX || clientX, y: pageY || clientY })
   }
 
-  const handleMouseLeave = () => {
+  const handleStart = (e: globalThis.TouchEvent | globalThis.MouseEvent) => {
+    setIsMouseDown(true)
+    updateMousePos(e)
+  }
+
+  const handleMove = throttle(updateMousePos, 50)
+
+  const handleEnd = () => {
     setIsMouseDown(false)
   }
 
   useEffect(() => {
-    targetRef && targetRef.addEventListener('mousedown', handleMouseDown)
-    targetRef && targetRef.addEventListener('touchstart', handleTouchStart)
-    window.addEventListener('touchmove', handleTouchMove)
-    window.addEventListener('touchend', handleMouseUp)
-    window.addEventListener('mouseup', handleMouseUp)
-    window.addEventListener('mousemove', handleMouseMove)
-    window.addEventListener('mouseleave', handleMouseLeave)
+    targetRef && targetRef.addEventListener('mousedown', handleStart)
+    targetRef && targetRef.addEventListener('touchstart', handleStart)
+
+    if (isMouseDown) {
+      window.addEventListener('touchmove', handleMove)
+      window.addEventListener('touchend', handleEnd)
+      window.addEventListener('mouseup', handleEnd)
+      window.addEventListener('mousemove', handleMove)
+      window.addEventListener('mouseleave', handleEnd)
+    }
 
     return () => {
-      targetRef && targetRef.removeEventListener('mousedown', handleMouseDown)
-      targetRef && targetRef.removeEventListener('touchstart', handleTouchStart)
-      window.removeEventListener('touchmove', handleTouchMove)
-      window.removeEventListener('touchend', handleMouseUp)
-      window.removeEventListener('mouseup', handleMouseUp)
-      window.removeEventListener('mousemove', handleMouseMove)
-      window.removeEventListener('mouseleave', handleMouseLeave)
+      targetRef && targetRef.removeEventListener('mousedown', handleStart)
+      targetRef && targetRef.removeEventListener('touchstart', handleStart)
+
+      if (isMouseDown) {
+        window.removeEventListener('touchmove', handleMove)
+        window.removeEventListener('touchend', handleEnd)
+        window.removeEventListener('mouseup', handleEnd)
+        window.removeEventListener('mousemove', handleMove)
+        window.removeEventListener('mouseleave', handleEnd)
+      }
     }
   }, [isMouseDown, targetRef])
 
