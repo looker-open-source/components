ef2a1121c42bfade2ff9d2798afef0f2c75ee71b
Lara Harrow <laraharrow@google.com>
2020-12-01 18:49:01 +0000

    updategrid to work with safari and firefox

--- packages/components/src/Form/Fields/FieldInline.tsx
+++ packages/components/src/Form/Fields/FieldInline.tsx
@@ -27,7 +27,7 @@
 import React, { FC } from 'react'
 import styled from 'styled-components'
 import { Label } from '../Label/Label'
-import { Paragraph } from '../../Text'
+import { Paragraph, Span } from '../../Text'
 import { ValidationMessage } from '../ValidationMessage/ValidationMessage'
 import { Truncate } from '../../Truncate'
 import { FieldBaseProps } from './FieldBase'
@@ -45,58 +45,76 @@ interface FieldInlinePropsInternal extends FieldBaseProps {
 const FieldInlineLayout: FC<FieldInlinePropsInternal> = ({
   className,
   children,
+  description,
   detail,
   label,
   id,
   required,
   validationMessage,
-}) => (
-  <label className={className} htmlFor={id}>
-    <InputArea>{children}</InputArea>
-    <Label as="span">
-      <Truncate>{label}</Truncate>
-      {required && <RequiredStar />}
-    </Label>
-    <MessageArea id={`${id}-describedby`}>
-      {detail && (
-        <Paragraph fontSize="xsmall" variant="secondary">
-          {detail}
-        </Paragraph>
-      )}
-      {validationMessage ? <ValidationMessage {...validationMessage} /> : null}
-    </MessageArea>
-  </label>
-)
+}) => {
+  return (
+    <label className={className} htmlFor={id}>
+      {detail && <FieldDetail>{detail}</FieldDetail>}
+      <InputArea>{children}</InputArea>
+      <Label as="span">
+        <Truncate>{label}</Truncate>
+        {required && <RequiredStar />}
+      </Label>
+      <MessageArea id={`${id}-describedby`}>
+        {description && <FieldDescription>{description}</FieldDescription>}
+        {validationMessage ? (
+          <ValidationMessage {...validationMessage} />
+        ) : null}
+      </MessageArea>
+    </label>
+  )
+}
 
-const InputArea = styled.div`
-  grid-column: 1;
-  grid-row: 1;
-  padding-right: ${({ theme }) => theme.space.xsmall};
-  /* stylelint-disable  */
-  -ms-grid-column: 1;
-  -ms-grid-row: 1;
-  /* stylelint-enable */
+const FieldDetail = styled(Span)`
+  color: ${({ theme }) => theme.colors.text2};
+  font-size: ${({ theme }) => theme.fontSizes.xsmall};
+  padding-left: ${({ theme }) => theme.space.xsmall};
 `
-const MessageArea = styled.div`
-  grid-column: 2;
-  grid-row: 2;
-  /* stylelint-disable  */
-  -ms-grid-column: 2;
-  -ms-grid-row: 2;
-  /* stylelint-enable */
+
+const FieldDescription = styled(Paragraph)`
+  color: ${({ theme }) => theme.colors.text2};
+  font-size: ${({ theme }) => theme.fontSizes.xsmall};
 `
+
+const InputArea = styled.div``
+
+const MessageArea = styled.div``
 
 export const FieldInline = styled(FieldInlineLayout)`
-  align-items: center;
   display: grid;
-  grid-template-columns: auto 1fr;
+  grid-template-columns: auto auto auto auto;
+  justify-content: start;
   line-height: ${({ theme }) => theme.lineHeights.small};
-
-  /* stylelint-disable  */
+  /* stylelint-disable */
   display: -ms-grid;
-  -ms-grid-columns: auto 1fr;
+  -ms-grid-columns: auto auto auto auto;
   /* stylelint-enable */
-
+  ${FieldDetail} {
+    grid-column: 3;
+    grid-row: 1;
+    /* stylelint-disable */
+    -ms-grid-column: 3;
+    -ms-grid-column-span: 2;
+    -ms-grid-row: 1;
+    /* stylelint-enable */
+  }
+  ${InputArea} {
+    grid-column: 1;
+    grid-row: 1;
+    padding-left: ${({ theme: { space } }) => space.xsmall};
+    padding-right: ${({ theme: { space } }) => space.xsmall};
+    padding-top: ${({ theme: { space } }) => space.xxxsmall};
+    /* stylelint-disable */
+    -ms-grid-column: 1;
+    -ms-grid-column-span: 1;
+    -ms-grid-row: 1;
+    /* stylelint-enable */
+  }
   ${Label} {
     align-items: center;
     color: ${({ theme, disabled }) => disabled && theme.colors.text1};
@@ -105,11 +123,22 @@ export const FieldInline = styled(FieldInlineLayout)`
     font-weight: normal;
     grid-column: 2;
     grid-row: 1;
-    padding-right: ${({ theme }) => theme.space.xsmall};
-
-    /* stylelint-disable  */
+    width: fit-content;
+    /* stylelint-disable */
     -ms-grid-column: 2;
+    -ms-grid-column-span: 1;
     -ms-grid-row: 1;
+    /* stylelint-enable */
+  }
+  ${MessageArea} {
+    grid-column: 2;
+    grid-column-end: span 2;
+    grid-row: 2;
+    /* stylelint-disable */
+    -ms-grid-column: 2;
+    -ms-grid-column-end: span 2;
+    -ms-grid-column-span: 2;
+    -ms-grid-row: 2;
     /* stylelint-enable */
   }
 `
