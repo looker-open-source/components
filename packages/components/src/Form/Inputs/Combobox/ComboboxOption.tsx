b542cf923ff327a04eccdb021a7a030b9c6f4c92
Elliot <ElliotTheHuman@users.noreply.github.com>
2020-12-11 14:48:57 -0800

    Update packages/components/src/Form/Inputs/Combobox/ComboboxOption.tsx
    Co-authored-by: mdodgelooker <53451193+mdodgelooker@users.noreply.github.com>

--- packages/components/src/Form/Inputs/Combobox/ComboboxOption.tsx
+++ packages/components/src/Form/Inputs/Combobox/ComboboxOption.tsx
@@ -145,15 +145,10 @@ export const ComboboxOptionWrapper = styled(ComboboxOptionWrapperInternal)`
   background-color: ${({ isSelected, theme }) =>
     isSelected && theme.colors.keySubtle};
 
-  &:hover {
+  &[aria-selected='true'] {
     background-color: ${({ isSelected, theme }) =>
       isSelected ? theme.colors.keyAccent : theme.colors.ui1};
   }
-
-  /** Essentially on hover and when "selected" */
-  /* &[aria-selected='true'] {
-    background-color: ${(props) => props.theme.colors.ui1};
-  } */
 `
 
 const ComboboxOptionInternal = forwardRef(
