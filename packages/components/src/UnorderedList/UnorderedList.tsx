d0db7ac5af41d3ba6c7feba0004ce5afe1c64ff4
Elliot Park <elliotpark@google.com>
2021-01-06 01:02:02 +0000

    Added UnorderedList suite

---
+++ packages/components/src/UnorderedList/UnorderedList.tsx
@@ -0,0 +1,86 @@
+/*
+
+ MIT License
+
+ Copyright (c) 2020 Looker Data Sciences, Inc.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ */
+
+import {
+  CompatibleHTMLProps,
+  reset,
+  space,
+  SpaceProps,
+  shouldForwardProp,
+  typography,
+  TypographyProps,
+  PositionProps,
+  LayoutProps,
+  position,
+  layout,
+} from '@looker/design-tokens'
+import styled from 'styled-components'
+import { variant } from 'styled-system'
+
+export interface UnorderedListProps
+  extends CompatibleHTMLProps<HTMLUListElement>,
+    PositionProps,
+    LayoutProps,
+    SpaceProps,
+    TypographyProps {
+  /**
+   * Specify the type of marker to place next to list items.
+   *
+   * @default 'none'
+   */
+  type?: 'none' | 'bullet'
+}
+
+const typeVariant = variant({
+  prop: 'type',
+  variants: {
+    bullet: {
+      listStyleType: 'disc',
+      pl: 'medium',
+    },
+    none: {
+      listStyleType: 'none',
+    },
+  },
+})
+
+export const UnorderedList = styled.ul
+  .withConfig({ shouldForwardProp })
+  .attrs<UnorderedListProps>(({ type = 'none' }) => ({
+    type: type || 'none',
+  }))<UnorderedListProps>`
+  ${reset}
+  ${typography}
+  ${typeVariant}
+  ${space}
+
+  ${position}
+  ${layout}
+
+  li {
+    margin-bottom: ${({ theme }) => theme.space.xxsmall};
+  }
+`
