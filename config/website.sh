9992c22e6c21f282d232c22c3127ad26329f4788
Luke Bowerman <34253496+lukelooker@users.noreply.github.com>
2021-01-22 14:40:07 -0800

    chore: Correct canary workflow configuration (#1808)

--- config/website.sh
+++ config/website.sh
@@ -14,5 +14,7 @@ mv www/public docs/$VERSION
 # Build Storybooks
 
 # @TODO Check if storybook is already built and re-use
-[ -d "storybook/storybook-static" ] && yarn workspace storybook build
+if [ ! -d storybook/storybook-static ]; then
+  yarn workspace storybook build
+fi
 cp -r storybook/storybook-static docs/$VERSION/storybook
