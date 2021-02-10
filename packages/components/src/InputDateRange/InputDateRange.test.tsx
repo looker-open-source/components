0340c288d7aaf0de50099129966636683d4c75fb
Daniel Christopher <dbchristopher@gmail.com>
2021-02-10 14:14:09 -0800

    test: InputDateRange tests (#1885)

--- packages/components/src/InputDateRange/InputDateRange.test.tsx
+++ packages/components/src/InputDateRange/InputDateRange.test.tsx
@@ -24,9 +24,10 @@
 
  */
 
-import React from 'react'
-import { fireEvent } from '@testing-library/react'
+import React, { useState } from 'react'
+import { fireEvent, screen } from '@testing-library/react'
 import { renderWithTheme } from '@looker/components-test-utils'
+import { RangeModifier } from 'react-day-picker'
 import { Locales } from '../utils/i18n'
 import { InputDateRange } from './InputDateRange'
 
@@ -43,6 +44,103 @@ afterEach(() => {
   jest.clearAllMocks()
 })
 
+const ControlledInputDateRange = () => {
+  const [value, setValue] = useState<Partial<RangeModifier> | undefined>({
+    from: new Date('June 3, 2019'),
+    to: new Date('June 9, 2019'),
+  })
+  return (
+    <>
+      <button
+        onClick={() =>
+          setValue({
+            from: new Date('June 5, 2019'),
+            to: new Date('June 15, 2019'),
+          })
+        }
+      >
+        June 5 - 15, 2019
+      </button>
+      <button
+        onClick={() =>
+          setValue({
+            from: new Date('January 1, 2012'),
+            to: new Date('February 15, 2012'),
+          })
+        }
+      >
+        January 1 - February 15, 2012
+      </button>
+      <button onClick={() => setValue({ from: new Date('February 9, 2021') })}>
+        From: February 9, 2021
+      </button>
+      <InputDateRange
+        value={value}
+        onChange={(date?: Partial<RangeModifier> | undefined) => setValue(date)}
+      />
+    </>
+  )
+}
+
+test('value can be controlled externally', () => {
+  renderWithTheme(<ControlledInputDateRange />)
+
+  expect(screen.getByDisplayValue('06/03/2019')).toBeInTheDocument()
+  fireEvent.click(screen.getByText('June 5 - 15, 2019')) // helper isDateRangeInView returns true
+  expect(screen.getByDisplayValue('06/15/2019')).toBeInTheDocument()
+  fireEvent.click(screen.getByText('January 1 - February 15, 2012')) // helper isDateRangeInView returns false
+  expect(screen.getByDisplayValue('01/01/2012')).toBeInTheDocument()
+})
+
+test('user can change the selected date via text input field', () => {
+  renderWithTheme(<ControlledInputDateRange />)
+
+  expect(screen.getByText('June 2019')).toBeInTheDocument()
+
+  const FromTextInput = screen.getByDisplayValue('06/03/2019')
+  const ToTextInput = screen.getByDisplayValue('06/09/2019')
+  fireEvent.focus(ToTextInput) // execute helper toggleActiveDateInput
+  fireEvent.blur(ToTextInput) // execute helper toggleActiveDateInput
+  fireEvent.focus(FromTextInput) // execute helper toggleActiveDateInput
+  fireEvent.change(FromTextInput, { target: { value: '01/01/2012' } })
+  fireEvent.blur(FromTextInput) // update value on blur
+
+  expect(screen.getByText('January 2012')).toBeInTheDocument()
+})
+
+test('navigates from month to month', () => {
+  const mockProps = {
+    onChange: jest.fn(),
+    value: {
+      from: new Date('June 3, 2019'),
+      to: new Date('June 9, 2019'),
+    },
+  }
+  renderWithTheme(<InputDateRange {...mockProps} />)
+  expect(screen.getByText('June 2019')).toBeInTheDocument()
+  fireEvent.click(screen.getByText('Previous Month'))
+  expect(screen.getByText('May 2019')).toBeInTheDocument()
+  fireEvent.click(screen.getByText('May 2019')) // jump to "now" (mocked date.now: February 2020)
+  fireEvent.click(screen.getByText('Next Month'))
+  expect(screen.getByText('March 2020')).toBeInTheDocument()
+})
+
+test('navigates from month to month by clicking calendar overflow dates', () => {
+  // "calendar overflow" refers to greyed out days that aren't technically
+  // in the currently viewed month
+  const mockProps = {
+    onChange: jest.fn(),
+    value: {
+      from: new Date('June 3, 2019'),
+      to: new Date('June 9, 2019'),
+    },
+  }
+  renderWithTheme(<InputDateRange {...mockProps} />)
+  expect(screen.getByText('June 2019')).toBeInTheDocument()
+  fireEvent.click(screen.getAllByText('26')[0]) // May 26th
+  expect(screen.getByText('May 2019')).toBeInTheDocument()
+})
+
 test('calls onChange prop when a day is clicked', () => {
   const mockProps = {
     defaultValue: {
@@ -51,15 +149,33 @@ test('calls onChange prop when a day is clicked', () => {
     },
     onChange: jest.fn(),
   }
-  const { getAllByText } = renderWithTheme(<InputDateRange {...mockProps} />)
+  renderWithTheme(<InputDateRange {...mockProps} />)
   expect(mockProps.onChange).not.toHaveBeenCalled()
 
-  const date = getAllByText('15')[0] // the 15th day of the month
-  fireEvent.click(date)
-  expect(mockProps.onChange).toHaveBeenCalledWith({
-    from: new Date('June 3, 2019'),
-    to: new Date('June 15, 2019 12:00'),
-  })
+  fireEvent.click(screen.getAllByText('4')[0]) // the 4th day of the month
+  fireEvent.click(screen.getAllByText('21')[0]) // the 21st day of the month
+  expect(mockProps.onChange.mock.calls).toMatchInlineSnapshot(`
+    Array [
+      Array [
+        Object {
+          "from": 2019-06-04T12:00:00.000Z,
+          "to": 2019-06-09T00:00:00.000Z,
+        },
+      ],
+      Array [
+        Object {
+          "from": 2019-06-04T12:00:00.000Z,
+          "to": 2019-06-21T12:00:00.000Z,
+        },
+      ],
+    ]
+  `)
+})
+
+test('gracefully accepts partial date range objects', () => {
+  renderWithTheme(<ControlledInputDateRange />)
+  fireEvent.click(screen.getByText('From: February 9, 2021')) // helper isDateRangeInView returns false
+  expect(screen.getByText('February 2021')).toBeInTheDocument()
 })
 
 test('selects a single day when clicking on one of the date endpoints', () => {
@@ -79,6 +195,62 @@ test('selects a single day when clicking on one of the date endpoints', () => {
   expect(mockProps.onChange).toHaveBeenCalledWith({
     from: new Date('June 3, 2019 12:00'),
     to: new Date('June 3, 2019 12:00'),
+  })
+})
+
+it('user can clear the selected date by deleting text input content', () => {
+  const mockProps = {
+    defaultValue: {
+      from: new Date('June 3, 2019 12:00'),
+      to: new Date('June 9, 2019 12:00'),
+    },
+    onChange: jest.fn(),
+  }
+  renderWithTheme(<InputDateRange {...mockProps} />)
+  const FromTextInput = screen.getByTestId('date-from-text-input')
+  fireEvent.focus(FromTextInput)
+  fireEvent.change(FromTextInput, { target: { value: '' } })
+  fireEvent.blur(FromTextInput) // update value on blur
+  const ToTextInput = screen.getByTestId('date-to-text-input')
+  fireEvent.focus(ToTextInput)
+  fireEvent.change(ToTextInput, { target: { value: '' } })
+  fireEvent.blur(ToTextInput) // update value on blur
+
+  expect(mockProps.onChange.mock.calls).toMatchInlineSnapshot(`
+    Array [
+      Array [
+        Object {
+          "from": 2019-06-09T12:00:00.000Z,
+          "to": 2019-06-09T12:00:00.000Z,
+        },
+      ],
+      Array [
+        Object {
+          "from": undefined,
+          "to": undefined,
+        },
+      ],
+    ]
+  `)
+})
+
+test('extends date range when clicking before current "from" endpoint', () => {
+  const mockProps = {
+    defaultValue: {
+      from: new Date('June 8, 2019 12:00'),
+      to: new Date('June 9, 2019 12:00'),
+    },
+    onChange: jest.fn(),
+  }
+  const { getAllByText } = renderWithTheme(<InputDateRange {...mockProps} />)
+  expect(mockProps.onChange).not.toHaveBeenCalled()
+
+  const newDate = getAllByText('1')[0] // the 1st day of the month (before default date range)
+  fireEvent.click(newDate)
+  // both from and to were set to June 3rd
+  expect(mockProps.onChange).toHaveBeenCalledWith({
+    from: new Date('June 1, 2019 12:00'),
+    to: new Date('June 9, 2019 12:00'),
   })
 })
 
