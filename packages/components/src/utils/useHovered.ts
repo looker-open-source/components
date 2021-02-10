4c6899e7218063bf8a310635b50de249cbffa87b
Elliot Park <elliotpark@google.com>
2021-02-10 19:42:59 +0000

    Added clarification comment about useHovered

--- packages/components/src/utils/useHovered.ts
+++ packages/components/src/utils/useHovered.ts
@@ -28,8 +28,9 @@ import { RefObject, useEffect, useState } from 'react'
 import { getCurrentNode } from './getCurrentNode'
 import { useCallbackRef } from './useCallbackRef'
 
-// Get the current hover state of an element
-// Recommended to be used with useCallbackRef
+/**
+ * Get the current hover / focus state of a element of which component has no control
+ */
 export function useHovered<E extends HTMLElement = HTMLElement>(
   hoverElement?: E | null | RefObject<E>
 ): [boolean, ((node: E | null) => void) | null] {
