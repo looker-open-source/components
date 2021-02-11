10b94349fac27ba8dca812963dfc62c78a5b8ce1
Luke Bowerman <lukebowerman@google.com>
2021-02-11 21:20:50 +0000

    PR feedback

--- packages/components/src/Form/Inputs/InputHidden/InputHidden.test.tsx
+++ packages/components/src/Form/Inputs/InputHidden/InputHidden.test.tsx
@@ -26,21 +26,20 @@
 
 import React from 'react'
 import { renderWithTheme } from '@looker/components-test-utils'
+import { screen } from '@testing-library/react'
 import { InputHidden } from './InputHidden'
 
 describe('InputHidden', () => {
   test('name and id', () => {
-    const { container } = renderWithTheme(<InputHidden name="Bob" id="Bobby" />)
-    const input = container.querySelector('input')
+    renderWithTheme(<InputHidden name="Bob" id="Bobby" defaultValue="bob" />)
+    const input = screen.getByDisplayValue('bob')
     expect(input).toHaveAttribute('name', 'Bob')
     expect(input).toHaveAttribute('id', 'Bobby')
   })
 
   test('with a value', () => {
-    const { container } = renderWithTheme(
-      <InputHidden defaultValue="Some value" />
-    )
-    const input = container.querySelector('input')
-    expect(input).toHaveValue('Some value')
+    renderWithTheme(<InputHidden defaultValue="Some value" />)
+
+    expect(screen.getByDisplayValue('Some value')).toHaveValue('Some value')
   })
 })
--- packages/components/src/Form/Inputs/TextArea/TextArea.test.tsx
+++ packages/components/src/Form/Inputs/TextArea/TextArea.test.tsx
@@ -31,41 +31,39 @@ import { TextArea } from './TextArea'
 
 describe('TextArea', () => {
   test('with placeholder', () => {
-    renderWithTheme(
-      <TextArea id="TextAreaID" placeholder="this is a placeholder" />
-    )
+    renderWithTheme(<TextArea placeholder="this is a placeholder" />)
     expect(
       screen.getByPlaceholderText('this is a placeholder')
     ).toBeInTheDocument()
   })
 
   test('should accept disabled', () => {
-    renderWithTheme(<TextArea disabled id="TextAreaID" />)
+    renderWithTheme(<TextArea disabled />)
     expect(screen.getByRole('textbox')).toBeDisabled()
   })
 
   test('with an error validation', () => {
-    renderWithTheme(<TextArea id="TextAreaID" validationType="error" />)
+    renderWithTheme(<TextArea validationType="error" />)
     expect(screen.getByRole('textbox')).toBeInvalid()
   })
 
   test('TextArea resizes with prop resize = true', () => {
-    renderWithTheme(<TextArea id="TextAreaID" resize />)
+    renderWithTheme(<TextArea resize />)
     expect(screen.getByRole('textbox')).toHaveStyle('resize: vertical')
   })
 
   test('TextArea resizes with prop resize = false', () => {
-    renderWithTheme(<TextArea id="TextAreaID" resize={false} />)
+    renderWithTheme(<TextArea resize={false} />)
     expect(screen.getByRole('textbox')).toHaveStyle('resize: none')
   })
 
   test('TextArea resizes with prop resize = none', () => {
-    renderWithTheme(<TextArea id="TextAreaID" resize="none" />)
+    renderWithTheme(<TextArea resize="none" />)
     expect(screen.getByRole('textbox')).toHaveStyle('resize: none')
   })
 
   test('TextArea resizes with prop resize = vertical', () => {
-    renderWithTheme(<TextArea id="TextAreaID" resize="vertical" />)
+    renderWithTheme(<TextArea resize="vertical" />)
     expect(screen.getByRole('textbox')).toHaveStyle('resize: vertical')
   })
 })
