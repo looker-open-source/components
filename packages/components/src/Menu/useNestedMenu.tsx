5e0635bb962584dae0dd4d2765ae969918906ed1
Elliot Park <elliotpark@google.com>
2021-02-12 01:31:43 +0000

    Refactored nested menu to utilize density from parent MenuList / List

--- packages/components/src/Menu/useNestedMenu.tsx
+++ packages/components/src/Menu/useNestedMenu.tsx
@@ -38,6 +38,7 @@ import React, {
 import { DialogContext } from '../Dialog'
 import { usePopover } from '../Popover'
 import { useWrapEvent } from '../utils'
+import { ListItemContext } from '../List/ListItemContext'
 import { MenuList } from './MenuList'
 import { NestedMenuContext } from './NestedMenuProvider'
 import { NestedMenuSurface } from './NestedMenuSurface'
@@ -104,6 +105,7 @@ export const useNestedMenu = ({
   )
 
   const { closeModal } = useContext(DialogContext)
+  const { density } = useContext(ListItemContext)
 
   // Show the nestedMenu by updating the context with the id for this one
   // Hide it by updating the context to an empty string
@@ -191,7 +193,7 @@ export const useNestedMenu = ({
 
   const { popover, popperInstanceRef, domProps } = usePopover({
     content: (
-      <MenuList data-autofocus="true" {...listHandlers}>
+      <MenuList data-autofocus="true" density={density} {...listHandlers}>
         {nestedMenu}
       </MenuList>
     ),
