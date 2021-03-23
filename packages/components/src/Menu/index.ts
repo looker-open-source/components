ab31544d5ed817e81aca236940ce7df597adfcea
mdodgelooker <meredith.dodge@looker.com>
2021-03-23 05:55:12 +0000

    Revert menu index.ts change

--- packages/components/src/Menu/index.ts
+++ packages/components/src/Menu/index.ts
@@ -25,8 +25,7 @@
  */
 
 export * from './Menu'
-export * from './MenuDivider'
 export * from './MenuList'
 export * from './MenuItem'
-export * from '../List/ListDivider'
+export * from './MenuDivider'
 export * from './MenuHeading'
