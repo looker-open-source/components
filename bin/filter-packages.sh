e9b2565bc17b6d4926cbd1e124577f7ad4aa1f39
mdodgelooker <meredith.dodge@looker.com>
2021-04-20 19:29:44 +0000

    Bash script

---
+++ bin/filter-packages.sh
@@ -0,0 +1,16 @@
+
+names='expressions components'
+sdkVersion='21.4.0'
+
+
+for name in $names
+do
+  # remove filter packages
+  rm -rf packages/filter-$name
+  # copy over filter packages
+  cp -r ../helltool/packages/filter-$name packages/
+  # update package.json
+  sed "s/@looker\/sdk\": \"*/@looker\/sdk\": \"$sdkVersion/g" packages/filter-$name/package.json
+done
+
+# update package.json
