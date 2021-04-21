7c28096e0d55415de9aa50ecf76b297998a52c44
mdodgelooker <meredith.dodge@looker.com>
2021-04-21 03:31:10 +0000

    Update bash script

--- bin/filter-packages.sh
+++ bin/filter-packages.sh
@@ -1,6 +1,7 @@
 
 names='expressions components'
 sdkVersion='21.4.0'
+localPackages='components components-providers icons'
 
 
 for name in $names
@@ -9,8 +10,16 @@ do
   rm -rf packages/filter-$name
   # copy over filter packages
   cp -r ../helltool/packages/filter-$name packages/
-  # update package.json
+  # use version # for @looker/sdk in package.json
   sed -i "s/@looker\/sdk\": \"\*/@looker\/sdk\": \"$sdkVersion/g" packages/filter-$name/package.json
+  # remove @looker/cli
+  sed -i '/"@looker\/cli": "\*",/d' packages/filter-$name/package.json
+
+  # use * for local packages in package.json
+  for package in $localPackages
+  do
+    sed -i "s/@looker\/$package\": \".*\"/@looker\/$package\": \"\*\"/g" packages/filter-$name/package.json
+  done
 done
 
 # update package.json
