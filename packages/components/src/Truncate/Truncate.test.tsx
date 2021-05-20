83dd55fb1cae543d8b620107da0c25c002f3f48c
Luke Bowerman <34253496+lukelooker@users.noreply.github.com>
2021-05-20 14:55:29 -0700

    feat(Truncate): now supports an optional `description` property (#2403)
    Adds new `description` option for `Truncate` and implements access to it via ListItem and descendants.

---
+++ packages/components/src/Truncate/Truncate.test.tsx
@@ -0,0 +1,105 @@
+/*
+
+ MIT License
+
+ Copyright (c) 2021 Looker Data Sciences, Inc.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ */
+
+import React from 'react'
+import { renderWithTheme } from '@looker/components-test-utils'
+import { act, fireEvent, screen } from '@testing-library/react'
+import { Box } from '../Layout/Box'
+import { Truncate } from './Truncate'
+
+const longLabel = 'This is a long label that should trigger truncation'
+
+describe('Truncate', () => {
+  let rafSpy: jest.SpyInstance<number, [FrameRequestCallback]>
+
+  beforeEach(() => {
+    jest.useFakeTimers()
+    rafSpy = jest
+      .spyOn(window, 'requestAnimationFrame')
+      .mockImplementation((cb: any) => cb())
+  })
+
+  afterEach(() => {
+    jest.useRealTimers()
+    rafSpy.mockRestore()
+  })
+
+  const runTimers = () =>
+    act(() => {
+      jest.runOnlyPendingTimers()
+    })
+
+  test('Basic', () => {
+    renderWithTheme(<Truncate>Hello world</Truncate>)
+    const trigger = screen.getByText('Hello world')
+    fireEvent.mouseOver(trigger)
+
+    runTimers()
+    const tooltip = screen.getAllByText('Hello world')
+    expect(tooltip.length).toEqual(1)
+  })
+
+  /**
+   * JSDom doesn't replicate browser enough to trigger a measurable overflow
+   * event.
+   *
+   * Need to figure out a clever way to trigger overflow so test can prove
+   * the tooltip is enabled in overflow scenario
+   */
+  xtest('Truncate active', () => {
+    renderWithTheme(
+      <Box width="5rem">
+        <Truncate>{longLabel}</Truncate>
+      </Box>
+    )
+
+    const trigger = screen.getAllByText(longLabel)[0]
+    fireEvent.mouseOver(trigger)
+
+    runTimers()
+    const tooltip = screen.getAllByText(longLabel)[1]
+    expect(tooltip).toBeVisible()
+
+    fireEvent.mouseOut(tooltip)
+    expect(tooltip).not.toBeInTheDocument()
+  })
+
+  test('Truncate detail', () => {
+    renderWithTheme(
+      <Truncate description="description text">Hello World</Truncate>
+    )
+
+    const trigger = screen.getByText('Hello World')
+    fireEvent.mouseOver(trigger)
+
+    runTimers()
+    const tooltip = screen.getByText('description text')
+    expect(tooltip).toBeVisible()
+
+    fireEvent.mouseOut(tooltip)
+    expect(tooltip).not.toBeInTheDocument()
+  })
+})
