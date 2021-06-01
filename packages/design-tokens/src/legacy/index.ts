04587a0a6148c84ec113d90d90b93eacb5b5d29c
Luke Bowerman <34253496+lukelooker@users.noreply.github.com>
2021-06-01 10:35:07 -0700

    feat(palette): No longer export palette, mark all as deprecated (#2445)
    Palette can be deep-imported during 2.x series but may be removed entirely in 3.x
    
    ```import { palette} from '@looker/design-tokens/lib/legacy'```
    
    BREAKING CHANGE: Design-tokens `palette` is no longer exported from root, deprecated for 3.x

--- packages/design-tokens/src/legacy/index.ts
+++ packages/design-tokens/src/legacy/index.ts
@@ -25,4 +25,9 @@
  */
 
 import * as palette from './palette'
+
+/**
+ * @todo - remove in 3.x
+ * @deprecated Palette support is now deprecated and will be removed in 3.x release
+ */
 export { palette }
