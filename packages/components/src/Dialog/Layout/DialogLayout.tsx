c3a06625d442836ec832def2f41b4ddef544a355
Lara Harrow <23616264+laraharrow@users.noreply.github.com>
2021-06-24 18:15:41 -0600

    fix(DialogLayout): `footerSecondary` is only valid if `footer` is also specified (#2528)
    * update DialogLayout to have union-tipy and prevent footerSecondary to be passed if footer is undefined
    
    * TS Build workaround
    
    Co-authored-by: Luke Bowerman <lukebowerman@google.com>

--- packages/components/src/Dialog/Layout/DialogLayout.tsx
+++ packages/components/src/Dialog/Layout/DialogLayout.tsx
@@ -35,30 +35,46 @@ import { DialogContent } from './DialogContent'
 import { DialogFooter } from './DialogFooter'
 import { DialogHeader, DialogHeaderProps } from './DialogHeader'
 
-export interface DialogLayoutProps extends ModalLayoutProps {
+type WithFooter = {
+  /**
+   * displays content on the right-side of the DialogFooter, uses
+   * Space to add whitespace between each element.
+   */
+  footer: ModalLayoutProps['footer']
   /**
    * Secondary content to place in the footer
    * NOTE: `footer` property must be supplied for footer to be displayed. Supplying
    * only `footerSecondary` will prevent `DialogFooter` from being displayed.
    */
   footerSecondary?: ReactNode
-
-  /**
-   * Content in header. If a `string` is supplied the content will be placed in a `<Header />`
-   */
-  header?: ReactNode
-
-  /**
-   * Replaces the built-in `IconButton` (generally used for close) with an arbitrary ReactNode
-   */
-  headerDetail?: ModalHeaderProps['detail']
-
-  /**
-   * Display "Close" IconButton in the DialogHeader.
-   * NOTE: `true` if no footer is supplied and `headerClose` is not explicitly specified.
-   */
-  headerCloseButton?: boolean
 }
+
+type WithoutFooter = {
+  footer?: undefined
+  footerSecondary?: never
+}
+
+type FooterOptions = WithFooter | WithoutFooter
+
+export type DialogLayoutProps = FooterOptions &
+  ModalLayoutProps & {
+    children?: ReactNode
+    /**
+     * Content in header. If a `string` is supplied the content will be placed in a `<Header />`
+     */
+    header?: ReactNode
+
+    /**
+     * Replaces the built-in `IconButton` (generally used for close) with an arbitrary ReactNode
+     */
+    headerDetail?: ModalHeaderProps['detail']
+
+    /**
+     * Display "Close" IconButton in the DialogHeader.
+     * NOTE: `true` if no footer is supplied and `headerClose` is not explicitly specified.
+     */
+    headerCloseButton?: boolean
+  }
 
 const constructDialogHeader = (
   children?: ReactNode,
@@ -88,9 +104,9 @@ export const DialogLayout: FC<DialogLayoutProps> = ({
   headerDetail,
   isLoading,
 }) => {
-  const dialogFooter = (footer || footerSecondary) && (
+  const dialogFooter = footer ? (
     <DialogFooter secondary={footerSecondary}>{footer}</DialogFooter>
-  )
+  ) : null
   const dialogHeader = constructDialogHeader(
     header,
     headerCloseButton,
