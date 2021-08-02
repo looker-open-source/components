95d24359157fcad38cc44f405d12c463efc82db2
Elliot Park <elliotpark@google.com>
2021-08-02 18:12:56 +0000

    Added label toggle behavior to NavTree

--- packages/components/src/NavTree/NavTree.tsx
+++ packages/components/src/NavTree/NavTree.tsx
@@ -24,18 +24,279 @@
 
  */
 
-import React, { useContext } from 'react'
-import styled from 'styled-components'
 import { SpacingSizes } from '@looker/design-tokens'
-import { TreeContext } from '../Tree/TreeContext'
-import { ListItemDetail, listItemDimensions } from '../ListItem'
-import { Tree, TreeProps } from '../Tree'
-import { AccordionIndicator } from '../Accordion2/AccordionIndicator'
-import { ControlledLoosely } from '../Accordion2/controlTypes'
+import styled from 'styled-components'
+import React, { cloneElement, FocusEvent, useContext, useState } from 'react'
 import { GenericClickProps } from '../utils/useClickable'
+import {
+  ControlledLoosely,
+  ControlledOrUncontrolled,
+} from '../Accordion2/controlTypes'
+import { AccordionIndicator } from '../Accordion2/AccordionIndicator'
+import { TreeProps, treeItemInnerPropKeys } from '../Tree'
+import {
+  ListItemDetail,
+  listItemDimensions,
+  ListItemContext,
+  ListItemProps,
+} from '../ListItem'
+import { TreeContext } from '../Tree/TreeContext'
+import { useAccordion2 } from '../Accordion2'
+import { Flex } from '../Layout'
+import {
+  createSafeRel,
+  getNextFocusTarget,
+  HoverDisclosureContext,
+  partitionAriaProps,
+  undefinedCoalesce,
+  useWrapEvent,
+} from '../utils'
+import { List } from '../List'
+import { createListItemPartitions } from '../ListItem/utils'
+import { indicatorDefaults } from '../Tree/utils'
+import { WindowedTreeContext } from '../Tree/WindowedTreeNode'
+import { TreeItemContent } from '../Tree/TreeItemContent'
+import { TreeItemLabel } from '../Tree/TreeItemLabel'
 import { NavTreeItemProps } from './NavTreeItem'
 
-const NavTreeStyle = styled(Tree).withConfig<
+/**
+ * Replicated Tree code
+ * TODO: When labelToggle is introduced the aria-* attributes should land on the nested ListItem's
+ * label container (i.e. the focusable element).
+ */
+const NavTreeLayout = ({
+  assumeIconAlignment,
+  border: propsBorder,
+  children,
+  className,
+  dividers,
+  forceLabelPadding,
+  isOpen: propsIsOpen,
+  itemRole = 'none', // By default, Tree's content container should be a 'div'
+  label,
+  labelBackgroundOnly: propsLabelBackgroundOnly,
+  defaultOpen,
+  onBlur,
+  onClose,
+  onFocus: propsOnFocus,
+  onOpen,
+  onMouseEnter,
+  onMouseLeave,
+  toggleOpen: propsToggleOpen,
+  ...restProps
+}: TreeProps) => {
+  const treeItemInnerProps = {}
+  const accordionInnerProps = {}
+  Object.entries(restProps).forEach((prop) => {
+    const [propKey, propValue] = prop
+    /**
+     * treeItemInnerPropKeys const assertion doesn't like checking against a string type;
+     * using "as ReadonlyArray<string>" to make the types happy
+     */
+    if (
+      restProps &&
+      (treeItemInnerPropKeys as ReadonlyArray<string>).includes(propKey)
+    ) {
+      treeItemInnerProps[propKey] = propValue
+    } else {
+      accordionInnerProps[propKey] = propValue
+    }
+  })
+
+  const {
+    color: propsColor,
+    density: propsDensity,
+    disabled,
+    href,
+    icon,
+    rel,
+    selected,
+    target,
+  } = treeItemInnerProps as Partial<ListItemProps>
+  const [ariaProps] = partitionAriaProps(restProps)
+
+  const [hovered, setHovered] = useState(false)
+  const handleWrapperMouseEnter = useWrapEvent(
+    () => setHovered(true),
+    onMouseEnter
+  )
+  const handleWrapperMouseLeave = useWrapEvent(
+    () => setHovered(false),
+    onMouseLeave
+  )
+
+  const listContext = useContext(ListItemContext)
+  const treeContext = useContext(TreeContext)
+
+  // Context for supporting windowing
+  // - density must be defined at the collection level for consistent child height
+  // - opened / closed state must be managed at the collection level for accurate item count
+  // - partialRender to hide the accordion disclosure if it's above the window
+  const {
+    density: collectionDensity,
+    isOpen: contextIsOpen,
+    toggleNode,
+    partialRender,
+  } = useContext(WindowedTreeContext)
+
+  const isOpen = contextIsOpen ?? propsIsOpen
+  const toggleOpen = toggleNode ?? propsToggleOpen
+
+  const border = undefinedCoalesce([propsBorder, treeContext.border])
+  const color = undefinedCoalesce([
+    propsColor,
+    treeContext.color,
+    listContext.color,
+  ])
+
+  const hasLabelBackgroundOnly = undefinedCoalesce([
+    propsLabelBackgroundOnly,
+    treeContext.labelBackgroundOnly,
+  ])
+  const startingDepth = 0
+  const depth = treeContext.depth ? treeContext.depth : startingDepth
+
+  const density = collectionDensity || propsDensity || treeContext.density || 0
+
+  const { indicatorIcons, indicatorPosition } = indicatorDefaults
+
+  const [inside, outside] = createListItemPartitions({
+    ...treeItemInnerProps,
+    children: label,
+    color,
+    density,
+    icon,
+  })
+  let accordionProps: ControlledOrUncontrolled = {
+    defaultOpen,
+    onClose,
+    onOpen,
+    ...accordionInnerProps,
+  }
+  if (isOpen !== undefined && toggleOpen) {
+    accordionProps = { ...accordionProps, isOpen, toggleOpen }
+  }
+
+  const {
+    contentDomProps,
+    domProps: { onFocus, ...restDomProps },
+    disclosureProps,
+    isOpen: accordionIsOpen,
+  } = useAccordion2({
+    'aria-selected': selected,
+    children: (
+      <List disableKeyboardNav role="group" windowing={false}>
+        {children}
+      </List>
+    ),
+    density,
+    disabled,
+    indicatorIcons,
+    indicatorPosition,
+    label: inside,
+    onBlur,
+    onFocus: useWrapEvent(() => setHovered(true), propsOnFocus),
+    role: 'treeitem',
+    tabIndex: -1,
+    ...accordionProps,
+  })
+
+  // This is needed so that hover disclosed elements don't get lost during keyboard nav
+  const handleWrapperBlur = (event: FocusEvent<HTMLElement>) => {
+    const nextFocusTarget = getNextFocusTarget(event)
+
+    if (
+      nextFocusTarget &&
+      !event.currentTarget.contains(nextFocusTarget as Node)
+    ) {
+      setHovered(false)
+    }
+  }
+
+  const {
+    indicator,
+    children: disclosureLabel,
+    onClick: onIndicatorClick,
+    onKeyUp: onIndicatorKeyUp,
+    ...disclosureDomProps
+  } = disclosureProps
+
+  // These additional props on indicator produce the "indicator toggle only" behavior
+  const renderedIndicator = cloneElement(indicator, {
+    onClick: onIndicatorClick,
+    onKeyUp: onIndicatorKeyUp,
+    tabIndex: -1,
+  })
+
+  const statefulProps = {
+    color,
+    disabled,
+    hovered,
+    selected,
+  }
+
+  return (
+    <HoverDisclosureContext.Provider value={{ visible: hovered }}>
+      <TreeContext.Provider
+        value={{
+          border,
+          color,
+          density,
+          depth: depth + 1,
+          labelBackgroundOnly: hasLabelBackgroundOnly,
+        }}
+      >
+        <div
+          {...restDomProps}
+          className={`${restDomProps.className} ${className}`}
+        >
+          {!partialRender && (
+            <Flex
+              as="li"
+              color="text5"
+              onBlur={handleWrapperBlur}
+              onMouseEnter={handleWrapperMouseEnter}
+              onMouseLeave={handleWrapperMouseLeave}
+            >
+              <TreeItemContent
+                aria-selected={selected}
+                depth={depth}
+                href={href}
+                itemRole={itemRole}
+                labelBackgroundOnly={hasLabelBackgroundOnly}
+                onFocus={onFocus}
+                rel={createSafeRel(rel, target)}
+                target={target}
+                {...ariaProps}
+                {...disclosureDomProps}
+                {...statefulProps}
+              >
+                {renderedIndicator}
+                {/**
+                 * @TODO: Delete labelBackgroundOnly behavior once FieldItem component is completed
+                 */}
+                {hasLabelBackgroundOnly ? (
+                  <TreeItemLabel {...statefulProps}>
+                    {disclosureLabel}
+                  </TreeItemLabel>
+                ) : (
+                  disclosureLabel
+                )}
+              </TreeItemContent>
+              {outside}
+            </Flex>
+          )}
+          {accordionIsOpen && <div {...contentDomProps} />}
+        </div>
+      </TreeContext.Provider>
+    </HoverDisclosureContext.Provider>
+  )
+}
+
+/**
+ * NavTree style overrides
+ */
+const NavTreeStyle = styled(NavTreeLayout).withConfig<
   {
     iconGap: SpacingSizes
     px: SpacingSizes
