2676b1140d179eaf9c51c8bd7deec19dcac99627
Spencer White <swhitey@google.com>
2021-08-03 10:00:16 -0700

    test(Badge): testing prop defaults (#2678)
    * test(Badge): testing prop defaults
    
    * fixingttest names

--- packages/components/src/Badge/Badge.test.tsx
+++ packages/components/src/Badge/Badge.test.tsx
@@ -28,13 +28,22 @@ import { renderWithTheme } from '@looker/components-test-utils'
 import { screen } from '@testing-library/react'
 import { Badge } from './Badge'
 
-test('Badge', () => {
-  renderWithTheme(
-    <Badge size="small" intent="positive">
-      Good!
-    </Badge>
-  )
-  const badge = screen.getByText('Good!')
-  expect(badge).toHaveStyle('background: rgb(228, 245, 235)')
-  expect(badge).toHaveStyle('line-height: 16px')
+describe('Badge', () => {
+  test('Defaults', () => {
+    renderWithTheme(<Badge>Good!</Badge>)
+    const badge = screen.getByText('Good!')
+    expect(badge).toHaveStyle('background: rgb(237, 232, 251)')
+    expect(badge).toHaveStyle('line-height: 24px')
+  })
+
+  test('Small, Positive', () => {
+    renderWithTheme(
+      <Badge size="small" intent="positive">
+        Good!
+      </Badge>
+    )
+    const badge = screen.getByText('Good!')
+    expect(badge).toHaveStyle('background: rgb(228, 245, 235)')
+    expect(badge).toHaveStyle('line-height: 16px')
+  })
 })
