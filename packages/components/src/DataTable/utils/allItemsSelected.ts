f9e352516bf4ca0b4129d3f046fe70d5ff787c61
Daniel Christopher <dbchristopher@google.com>
2021-08-03 17:17:49 +0000

    refactor out unreachable code path from allItemsSelected

--- packages/components/src/DataTable/utils/allItemsSelected.ts
+++ packages/components/src/DataTable/utils/allItemsSelected.ts
@@ -27,13 +27,17 @@
 import { MixedBoolean } from '../../Form/Inputs/Checkbox'
 import { SelectConfig } from '../types'
 
-export const allItemsSelected = (select?: SelectConfig): MixedBoolean => {
-  if (!select) return false
+type HelperArgs = Pick<SelectConfig, 'selectedItems' | 'pageItems'>
 
+export const allItemsSelected = (
+  select: HelperArgs = { pageItems: [], selectedItems: [] }
+): MixedBoolean => {
   const { selectedItems, pageItems } = select
 
-  if (selectedItems.length === 0) return false
-  else if (pageItems.every((id) => selectedItems.includes(id))) return true
-  else if (pageItems.some((id) => selectedItems.includes(id))) return 'mixed'
-  else return false
+  if (selectedItems.length > 0) {
+    if (pageItems.every((id) => selectedItems.includes(id))) return true
+    else if (pageItems.some((id) => selectedItems.includes(id))) return 'mixed'
+  }
+
+  return false
 }
