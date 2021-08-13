829436798fc6b07ff052a20e14f31c79f22af208
Lara <laraharrow@google.com>
2021-08-13 23:51:29 +0000

    All the work for Tabs2

---
+++ packages/components/src/Tabs2/TabList2.tsx
@@ -0,0 +1,90 @@
+/*
+
+ MIT License
+
+ Copyright (c) 2021 Looker Data Sciences, Inc.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ */
+
+import { useTranslation } from 'react-i18next'
+import React, { forwardRef, Ref } from 'react'
+import { fontSize, padding, reset } from '@looker/design-tokens'
+import styled, { css } from 'styled-components'
+import { useArrowKeyNav } from '../utils'
+import { TabList2Props } from './types'
+import { Tab2 } from './Tab2'
+
+const defaultLayoutCSS = css`
+  ${Tab2} {
+    min-width: 3rem;
+  }
+  ${Tab2} + ${Tab2} {
+    margin-left: ${(props) => props.theme.space.xlarge};
+  }
+`
+
+const distributeCSS = css`
+  display: grid;
+  grid-auto-columns: 1fr;
+  grid-auto-flow: column;
+  ${Tab2} {
+    padding: ${({ theme: { space } }) => `${space.xsmall} ${space.medium}`};
+  }
+`
+
+const TabList2CSS = css<TabList2Props>`
+  ${reset}
+  ${padding}
+  ${fontSize}
+  border-bottom: 1px solid ${(props) => props.theme.colors.ui2};
+  -ms-overflow-style: none; /* Internet Explorer 10+ */
+  overflow-x: auto;
+  scrollbar-width: none; /* Firefox */
+  white-space: nowrap;
+  &::-webkit-scrollbar {
+    display: none; /* Safari and Chrome */
+  }
+  ${({ distribute }) => (distribute ? distributeCSS : defaultLayoutCSS)}
+`
+
+export const TabList2 = styled(
+  forwardRef(
+    ({ children, className }: TabList2Props, ref: Ref<HTMLDivElement>) => {
+      const { t } = useTranslation('TabList')
+      const navProps = useArrowKeyNav({ axis: 'horizontal', ref })
+
+      return (
+        <div
+          aria-label={t('Tabs')}
+          className={className}
+          role="tablist"
+          {...navProps}
+        >
+          {children}
+        </div>
+      )
+    }
+  )
+).attrs(({ fontSize = 'small' }) => ({
+  fontSize,
+}))`
+  ${TabList2CSS}
+`
---
+++ packages/components/src/Tabs2/types.ts
@@ -0,0 +1,103 @@
+/*
+
+ MIT License
+
+ Copyright (c) 2021 Looker Data Sciences, Inc.
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+
+ */
+
+import { ReactElement } from 'react'
+import {
+  CompatibleHTMLProps,
+  FontSizeProps,
+  LayoutProps,
+  PaddingProps,
+  TypographyProps,
+} from '@looker/design-tokens'
+
+export type Tab2Props = Omit<CompatibleHTMLProps<HTMLButtonElement>, 'type'> &
+  LayoutProps &
+  PaddingProps &
+  TypographyProps & {
+    /**
+     * displays as the `Tab`'s value
+     */
+    label: string
+    /**
+     * callback to manage when `Tab` is clicked
+     */
+    onSelect?: () => void
+    /**
+     * specific `Tab` that is selected.
+     */
+    selected?: boolean
+  }
+
+export type TabList2Props = PaddingProps &
+  FontSizeProps & {
+    /**
+     * element that will be displayed as the `Tab` value
+     */
+    children: JSX.Element[]
+    className?: string
+    /**
+     * spread the collection of `Tab` on its full width.
+     */
+    distribute?: boolean
+  }
+
+type TabStackMember = Tab2Props & {
+  id: string
+}
+
+export type TabStack = TabStackMember[]
+
+type Controlled = {
+  /**
+   * Which tab to show on load
+   */
+  defaultTabId?: string
+  /**
+   * Controlled: which tab to show now
+   */
+  tabId?: string
+  /**
+   * Callback called when tabId changes
+   */
+  onTabChange?: (tabId: string) => void
+}
+
+type Uncontrolled = {
+  defaultTabId?: never
+  onTabChange?: never
+  tabId?: string
+}
+
+export type Tabs2Props = (Controlled | Uncontrolled) & {
+  /**
+   * The list of `Tab`
+   */
+  children: ReactElement<Tab2Props> | ReactElement<Tab2Props>[]
+  /**
+   * Spread the Tab between all the space available
+   */
+  distributed?: boolean
+}
