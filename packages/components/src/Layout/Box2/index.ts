467042e83437416eaa92f7f2cdc707936c700da7
Luke Bowerman <34253496+lukelooker@users.noreply.github.com>
2021-08-23 10:41:47 -0700

    feat(Box2): A new improved box in `Box2` (#2640)
    Also deprecates Box, Flex & FlexItem

--- packages/components/src/Layout/Box2/index.ts
+++ packages/components/src/Layout/Box2/index.ts
@@ -24,26 +24,4 @@
 
  */
 
-import type { FlexboxProps } from '@looker/design-tokens'
-import { flexbox, shouldForwardProp } from '@looker/design-tokens'
-import styled from 'styled-components'
-import type { ComplexLayoutProps } from '../utils/complex'
-import { complexLayoutCSS } from '../utils/complex'
-
-export interface FlexItemProps extends ComplexLayoutProps, FlexboxProps {}
-
-/**
- * @deprecated - Use `Box2` or `div` instead.
- * NOTE: It's quite possible you don't need `FlexItem` at all
- */
-export const FlexItem = styled.div.withConfig({
-  shouldForwardProp,
-})<FlexItemProps>`
-  ${complexLayoutCSS}
-  ${flexbox}
-  /*
-   * A min-width must be set here to resolve a firefox bug where any children
-   * with style of text-overflow: ellipsis; will otherwise not truncate the
-   * text appropriately. */
-  min-width: 0; /* IMPORTANT!! Do not delete! */
-`
+export * from './Box2'
