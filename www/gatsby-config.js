904330f412ad39e8711ac403bd565bc9a685ad4d
Luke Bowerman <lukebowerman@google.com>
2021-08-27 20:15:01 +0000

    Correct Gatsby build

--- www/gatsby-config.js
+++ www/gatsby-config.js
@@ -42,6 +42,7 @@ module.exports = {
     `gatsby-plugin-typescript`,
     'gatsby-plugin-react-helmet',
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-tsconfig-paths`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
