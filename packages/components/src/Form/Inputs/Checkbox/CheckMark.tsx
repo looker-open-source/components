743e32c72e4578b74466617ada3943567605b3ba
mdodgelooker <meredith.dodge@looker.com>
2021-09-08 17:11:58 +0000

    currentColor

--- packages/components/src/Form/Inputs/Checkbox/CheckMark.tsx
+++ packages/components/src/Form/Inputs/Checkbox/CheckMark.tsx
@@ -35,7 +35,7 @@ export const CheckMark = () => (
   >
     <path
       d="M5.78626 9.15812L2.20407 5.58594L0.805634 6.99219L5.78626 11.9881L14.7863 2.98812L13.3763 1.57812L5.78626 9.15812Z"
-      fill="white"
+      fill="currentColor"
     />
   </svg>
 )
