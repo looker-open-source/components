43375e936588589c07b31c16ee3403e5c2738d0b
Bryn Ryans <bryn.ryans@looker.com>
2021-09-09 21:41:30 +0000

    shortcodes

--- www/src/MDX/shortcodes.ts
+++ www/src/MDX/shortcodes.ts
@@ -1,3 +1,4 @@
 export * from '../documentation/foundations/demos'
+export * from '../documentation/components/content/demos'
 export * from '../documentation/components/date/demos'
 export * from '../documentation/components/ImportMessage'
