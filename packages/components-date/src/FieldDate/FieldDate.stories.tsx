1297a423f9b14d5b1393e3ea52979e7c8a8b5594
mdodgelooker <meredith.dodge@looker.com>
2021-09-29 22:03:41 +0000

    Refactor stories

--- packages/components-date/src/FieldDate/FieldDate.stories.tsx
+++ packages/components-date/src/FieldDate/FieldDate.stories.tsx
@@ -24,38 +24,21 @@
 
  */
 import type { Story } from '@storybook/react/types-6-0'
-import type { SyntheticEvent } from 'react'
 import React, { useState } from 'react'
 import {
   Button,
   Fieldset,
-  FieldSelect,
-  FieldSlider,
   i18nInit,
   Paragraph,
   Popover,
   PopoverContent,
 } from '@looker/components'
 
-import ar from 'date-fns/locale/ar-SA'
-import de from 'date-fns/locale/de'
-import en from 'date-fns/locale/en-US'
-import es from 'date-fns/locale/es'
-import fr from 'date-fns/locale/fr'
 import it from 'date-fns/locale/it'
-import ja from 'date-fns/locale/ja'
-import ko from 'date-fns/locale/ko'
-import nl from 'date-fns/locale/nl'
-import pl from 'date-fns/locale/pl'
-import pt from 'date-fns/locale/pt'
-import ptBr from 'date-fns/locale/pt-BR'
-import ru from 'date-fns/locale/ru'
-import sv from 'date-fns/locale/sv'
-import tr from 'date-fns/locale/tr'
-import zhCn from 'date-fns/locale/zh-CN'
-import zhTw from 'date-fns/locale/zh-TW'
 
 import { defaultArgTypes as argTypes } from '../../../../apps/storybook/src/defaultArgTypes'
+import type { DayOfWeekNumbers } from '../__mocks__/LocaleSettings'
+import { LocaleSettings } from '../__mocks__/LocaleSettings'
 import { DateFormat } from '../DateFormat'
 import type { FieldDateProps } from './FieldDate'
 import { FieldDate } from './FieldDate'
@@ -84,8 +67,6 @@ Error.args = {
   ...Basic.args,
   validationMessage: { message: 'Error Message', type: 'error' },
 }
-
-type DayOfWeekNumbers = 0 | 1 | 2 | 3 | 4 | 5 | 6
 
 export const Controlled = () => {
   const [controlledDate, setControlledDate] = useState<any>()
@@ -120,92 +101,33 @@ Controlled.parameters = {
   storyshots: { disable: true },
 }
 
-const dateFnLocaleMap = {
-  ar: ar,
-  de: de,
-  en: en,
-  es: es,
-  fr: fr,
-  it: it,
-  ja: ja,
-  ko: ko,
-  nl: nl,
-  pl: pl,
-  pt: pt,
-  'pt-br': ptBr,
-  ru: ru,
-  sv: sv,
-  tr: tr,
-  'zh-cn': zhCn,
-  'zh-tw': zhTw,
-}
-
 export const Localized = () => {
-  const [localizedDate, setLocalizedDate] = useState<Date | undefined>()
-  const [localeCode, setLocale] = useState('it')
-  const locale = dateFnLocaleMap[localeCode]
+  const [date, setDate] = useState<Date | undefined>()
+  const [locale, setLocale] = useState(it)
   const [firstDayOfWeek, setFirstDayOfWeek] = useState<DayOfWeekNumbers>(
-    locale.options?.weekStartsOn
+    locale.options?.weekStartsOn || 0
   )
-
-  const handleFirstDayOfWeekChange = (e: SyntheticEvent<HTMLInputElement>) => {
-    setFirstDayOfWeek(parseInt(e.currentTarget.value, 10) as DayOfWeekNumbers)
-  }
-
-  const handleLocaleChange = (val: any) => {
-    setLocale(val)
-    setLocalizedDate(undefined)
-    setFirstDayOfWeek(locale.options?.weekStartsOn)
-  }
+  const clearDate = () => setDate(undefined)
 
   return (
     <Fieldset>
       <Paragraph>
         Selected:
-        {localizedDate && (
-          <DateFormat locale={locale}>{localizedDate}</DateFormat>
-        )}
+        {date && <DateFormat locale={locale}>{date}</DateFormat>}
       </Paragraph>
 
       <FieldDate
-        onChange={setLocalizedDate}
+        onChange={setDate}
         firstDayOfWeek={firstDayOfWeek}
         locale={locale}
-        key={localeCode}
+        key={locale.code}
         m="small"
       />
-
-      <FieldSelect
-        label="Date String Format"
-        options={[
-          { label: 'Arabic (ar)', value: 'ar' },
-          { label: 'German (de)', value: 'de' },
-          { label: 'English (en)', value: 'en' },
-          { label: 'Spanish (es)', value: 'es' },
-          { label: 'French (fr)', value: 'fr' },
-          { label: 'Italian (it)', value: 'it' },
-          { label: 'Japanese (ja)', value: 'ja' },
-          { label: 'Korean (ko)', value: 'ko' },
-          { label: 'Dutch (nl)', value: 'nl' },
-          { label: 'Polish (pl)', value: 'pl' },
-          { label: 'Portuguese (pt)', value: 'pt' },
-          { label: 'Portuguese - Brazil (pt-br)', value: 'pt-br' },
-          { label: 'Russian (ru)', value: 'ru' },
-          { label: 'Swedish (sv)', value: 'sv' },
-          { label: 'Turkish (tr)', value: 'tr' },
-          { label: 'Chinese - China (zh-cn)', value: 'zh-cn' },
-          { label: 'Chinese - Taiwan (zh-tw)', value: 'zh-tw' },
-        ]}
-        value={localeCode}
-        onChange={handleLocaleChange}
-        width="auto"
-      />
-      <FieldSlider
-        label="First Day Of Week"
-        min={0}
-        max={6}
-        value={firstDayOfWeek}
-        onChange={handleFirstDayOfWeekChange}
+      <LocaleSettings
+        setLocale={setLocale}
+        firstDayOfWeek={firstDayOfWeek}
+        setFirstDayOfWeek={setFirstDayOfWeek}
+        clearDate={clearDate}
       />
     </Fieldset>
   )
--- packages/components-date/src/FieldDateRange/FieldDateRange.stories.tsx
+++ packages/components-date/src/FieldDateRange/FieldDateRange.stories.tsx
@@ -25,30 +25,10 @@
  */
 import React, { useState } from 'react'
 import type { Story } from '@storybook/react/types-6-0'
-import {
-  ExtendComponentsThemeProvider,
-  FieldSelect,
-  Paragraph,
-} from '@looker/components'
-
-import ar from 'date-fns/locale/ar-SA'
-import de from 'date-fns/locale/de'
-import en from 'date-fns/locale/en-US'
-import es from 'date-fns/locale/es'
-import fr from 'date-fns/locale/fr'
+import { ExtendComponentsThemeProvider, Paragraph } from '@looker/components'
 import it from 'date-fns/locale/it'
-import ja from 'date-fns/locale/ja'
-import ko from 'date-fns/locale/ko'
-import nl from 'date-fns/locale/nl'
-import pl from 'date-fns/locale/pl'
-import pt from 'date-fns/locale/pt'
-import ptBr from 'date-fns/locale/pt-BR'
-import ru from 'date-fns/locale/ru'
-import sv from 'date-fns/locale/sv'
-import tr from 'date-fns/locale/tr'
-import zhCn from 'date-fns/locale/zh-CN'
-import zhTw from 'date-fns/locale/zh-TW'
-
+import type { DayOfWeekNumbers } from '../__mocks__/LocaleSettings'
+import { LocaleSettings } from '../__mocks__/LocaleSettings'
 import { defaultArgTypes as argTypes } from '../../../../apps/storybook/src/defaultArgTypes'
 import { DateFormat } from '../DateFormat'
 import type { FieldInputDateRangeProps } from './FieldDateRange'
@@ -150,76 +130,29 @@ Error.args = {
   validationMessage: { message: 'Field Disabled', type: 'error' },
 }
 
-const dateFnLocaleMap = {
-  ar: ar,
-  de: de,
-  en: en,
-  es: es,
-  fr: fr,
-  it: it,
-  ja: ja,
-  ko: ko,
-  nl: nl,
-  pl: pl,
-  pt: pt,
-  'pt-br': ptBr,
-  ru: ru,
-  sv: sv,
-  tr: tr,
-  'zh-cn': zhCn,
-  'zh-tw': zhTw,
-}
-
 export const Localized = () => {
-  const startDate = new Date()
-  startDate.setDate(9)
-  const endDate = new Date()
-  endDate.setDate(15)
-
-  const [localizedDate, setLocalizedDate] = useState<DateRange | undefined>()
-  const [localeCode, setLocale] = useState('ko')
-  const locale = dateFnLocaleMap[localeCode]
-
-  const handleLocaleChange = (val: any) => {
-    setLocale(val)
-    setLocalizedDate(undefined)
-  }
+  const [range, setDateRange] = useState<DateRange | undefined>()
+  const [locale, setLocale] = useState(it)
+  const [firstDayOfWeek, setFirstDayOfWeek] = useState<DayOfWeekNumbers>(
+    locale.options?.weekStartsOn || 0
+  )
+  const clearDate = () => setDateRange(undefined)
 
   return (
     <>
       <Paragraph>
         Selected:
-        <DateFormat locale={locale}>{localizedDate?.from}</DateFormat>
+        <DateFormat locale={locale}>{range?.from}</DateFormat>
         {` - `}
-        <DateFormat locale={locale}>{localizedDate?.to}</DateFormat>
+        <DateFormat locale={locale}>{range?.to}</DateFormat>
       </Paragraph>
-      <FieldSelect
-        label="Locale"
-        options={[
-          { label: 'Arabic (ar)', value: 'ar' },
-          { label: 'German (de)', value: 'de' },
-          { label: 'English (en)', value: 'en' },
-          { label: 'Spanish (es)', value: 'es' },
-          { label: 'French (fr)', value: 'fr' },
-          { label: 'Italian (it)', value: 'it' },
-          { label: 'Japanese (ja)', value: 'ja' },
-          { label: 'Korean (ko)', value: 'ko' },
-          { label: 'Dutch (nl)', value: 'nl' },
-          { label: 'Polish (pl)', value: 'pl' },
-          { label: 'Portuguese (pt)', value: 'pt' },
-          { label: 'Portuguese - Brazil (pt-br)', value: 'pt-br' },
-          { label: 'Russian (ru)', value: 'ru' },
-          { label: 'Swedish (sv)', value: 'sv' },
-          { label: 'Turkish (tr)', value: 'tr' },
-          { label: 'Chinese - China (zh-cn)', value: 'zh-cn' },
-          { label: 'Chinese - Taiwan (zh-tw)', value: 'zh-tw' },
-        ]}
-        value={localeCode}
-        onChange={handleLocaleChange}
-        width="auto"
+      <FieldDateRange onChange={setDateRange} locale={locale} />
+      <LocaleSettings
+        setLocale={setLocale}
+        firstDayOfWeek={firstDayOfWeek}
+        setFirstDayOfWeek={setFirstDayOfWeek}
+        clearDate={clearDate}
       />
-
-      <FieldDateRange onChange={setLocalizedDate} locale={locale} />
     </>
   )
 }
